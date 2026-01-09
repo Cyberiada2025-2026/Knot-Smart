33587762715078ff2d0069b94ade8d9e6dd97d83
sajmon <szymon17099@gmail.com>
2026-01-10 00:31:02 +0100

    Add loading screen support

--- src/autoload/scene_manager.gd
+++ src/autoload/scene_manager.gd
@@ -1,7 +1,8 @@
 extends Node
 
 var current_scene = null
-
+var loading_scene = null
+var loading_screen = preload("uid://crhln4qdp4hph")
 
 func _ready():
 	current_scene = get_tree().current_scene
@@ -14,8 +15,19 @@ func goto_scene(path):
 func _deferred_goto_scene(path):
 	current_scene.free()
 
-	var scene = ResourceLoader.load(path)
-	current_scene = scene.instantiate()
+	# Params:
+	# "" - type hint - default value
+	# true - use_sub_threads - enables accelerated loading using multithreading
+	ResourceLoader.load_threaded_request(path, "", true)
+	loading_scene = loading_screen.instantiate()
+	loading_scene.set_path(path)
 
+	get_tree().root.add_child(loading_scene)
+	get_tree().current_scene = loading_scene
+
+	current_scene = await loading_scene.loaded_instance
 	get_tree().root.add_child(current_scene)
+
+	loading_scene.queue_free()
 	get_tree().current_scene = current_scene
+	

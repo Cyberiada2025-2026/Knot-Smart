8a297bd4f6ca61f2e299502ff75623e090b2565a
sajmon <szymon17099@gmail.com>
2026-01-09 22:44:15 +0100

    Add basic scene switching code

--- src/autoload/scene_manager.gd
+++ src/autoload/scene_manager.gd
@@ -1,1 +1,21 @@
 extends Node
+
+var current_scene = null
+
+
+func _ready():
+	current_scene = get_tree().current_scene
+
+
+func goto_scene(path):
+	_deferred_goto_scene.call_deferred(path) 
+
+
+func _deferred_goto_scene(path):
+	current_scene.free()
+
+	var scene = ResourceLoader.load(path)
+	current_scene = scene.instantiate()
+
+	get_tree().root.add_child(current_scene)
+	get_tree().current_scene = current_scene

e200cdf06764e6f856dd0029182d03db7abff968
ThreeCoffes <leonard.madzielewski@gmail.com>
2026-02-04 20:30:14 +0100

    run gdformat on all appropriate files

--- src/characters/player/player_camera.gd
+++ src/characters/player/player_camera.gd
@@ -4,7 +4,6 @@ class_name PlayerCamera
 
 signal camera_rotated(vector: Vector3, angle: float)
 
-
 @export_category("camera locations")
 @export var camera: Node3D
 @export var arm: SpringArm3D
@@ -33,34 +32,35 @@ func  _ready() -> void:
 	rotation_strategy.start()
 	view_strategy.start(self)
 
+
 func _process(delta: float) -> void:
 	self.global_position = get_parent().player_physics.global_position
 	_process_change_person(delta)
 	camera.rotation.y = 0
 	camera.rotation.z = 0
-	
+
 	## CAMERA ROTATION
 	if get_parent().is_rotating == false:
 		rotation_strategy.rotate(self, mouse_relative, delta)
 	mouse_relative = Vector2.ZERO
-	
+
 	## CAMERA ZOOM
 	view_strategy.zoom(self, delta)
 
+
 func _process_change_person(delta: float) -> void:
 	arm.spring_length = lerp(arm.spring_length, arm_length, person_change_speed * delta)
-
 
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("change_camera"):
 		view_strategy = view_strategy.next_strategy
 		view_strategy.change_view_to(self)
-	
+
 	if event.is_action_pressed("change_camera_mode"):
 		rotation_strategy = rotation_strategy.next_strategy
 		rotation_strategy.change_to()
-	
+
 	## CAMERA ROTATION
 	if event is InputEventMouseMotion:
 		mouse_relative += event.relative
@@ -71,6 +71,7 @@ func rotate_left_right(vector: Vector3, angle: float) -> void:
 		self.rotate(vector, angle)
 		camera_rotated.emit(vector, angle)
 
+
 func rotate_up_down(angle: float) -> void:
 	if view_strategy.should_rotate_up_down:
 		camera.rotate(Vector3.RIGHT, angle)
@@ -78,6 +79,7 @@ func rotate_up_down(angle: float) -> void:
 			camera.rotation.x = deg_to_rad(camera_up_rotation_limit)
 		elif camera.rotation.x < deg_to_rad(camera_down_rotation_limit):
 			camera.rotation.x = deg_to_rad(camera_down_rotation_limit)
+
 
 func get_normal() -> Vector3:
 	return get_parent().ground_normal
--- src/characters/player/player_physics.gd
+++ src/characters/player/player_physics.gd
@@ -9,8 +9,10 @@ class_name PlayerPhysics
 
 var player: Player
 
+
 func _ready() -> void:
 	player = self.get_parent()
+
 
 func _physics_process(delta: float) -> void:
 	_handle_movement(delta)
@@ -22,22 +24,30 @@ func _handle_movement(delta: float) -> void:
 	_convert_to_real()
 	move_and_slide()
 
+
 func _convert_to_flat() -> void:
-	var right: Vector3 = player.front.rotated(player.ground_normal, PI/2)
+	var right: Vector3 = player.front.rotated(player.ground_normal, PI / 2)
 	var real_velocity = velocity
 	velocity.x = real_velocity.dot(right)
 	velocity.y = real_velocity.dot(player.ground_normal)
 	velocity.z = real_velocity.dot(player.front)
 
+
 func _convert_to_real() -> void:
-	var right: Vector3 = player.front.rotated(player.ground_normal, PI/2)
+	var right: Vector3 = player.front.rotated(player.ground_normal, PI / 2)
 	var flat_velocity = velocity
-	velocity = (flat_velocity.x * right) + (flat_velocity.y * player.ground_normal) + (flat_velocity.z * player.front)
+	velocity = (
+		(flat_velocity.x * right)
+		+ (flat_velocity.y * player.ground_normal)
+		+ (flat_velocity.z * player.front)
+	)
+
 
 func _handle_flat_movement(delta: float) -> void:
 	_handle_gravity(delta)
 	_handle_jump()
 	_handle_move_input(delta)
+
 
 func _handle_move_input(delta: float):
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down").normalized()
@@ -45,12 +55,14 @@ func _handle_move_input(delta: float):
 		velocity.x = -input_dir.x * speed * delta
 		velocity.z = -input_dir.y * speed * delta
 	else:
-		velocity.x = move_toward(velocity.x, 0, slowing_speed*delta)
-		velocity.z = move_toward(velocity.z, 0, slowing_speed*delta)
+		velocity.x = move_toward(velocity.x, 0, slowing_speed * delta)
+		velocity.z = move_toward(velocity.z, 0, slowing_speed * delta)
+
 
 func _handle_jump():
 	if Input.is_action_just_pressed("jump_button") and is_on_floor():
 		velocity.y = jump_strength
+
 
 func _handle_gravity(delta: float):
 	if not is_on_floor():

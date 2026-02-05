a7aaf714e972251525a82a15061ee4fd5361b22b
ThreeCoffes <leonard.madzielewski@gmail.com>
2026-02-05 19:07:51 +0100

    gdformat

--- src/shared/buildings/building_generation/box_shape.gd
+++ src/shared/buildings/building_generation/box_shape.gd
@@ -9,10 +9,17 @@ extends Node
 @export_group("Other")
 @export var debug_color: Color
 
+
 func to_cell() -> Cell:
 	return Cell.new(start, end)
 
-func draw_visualization(global_position: Vector3, rotation: Quaternion, grid_scale: Vector3) -> void:
+
+func draw_visualization(
+	global_position: Vector3, rotation: Quaternion, grid_scale: Vector3
+) -> void:
 	var rotation_axis = rotation.get_axis() if rotation.get_angle() != 0 else Vector3.UP
-	var position: Vector3 = global_position + (start as Vector3 * grid_scale).rotated(rotation_axis, rotation.get_angle()) 
-	DebugDraw3D.draw_box(position, rotation, (end-start) as Vector3 * grid_scale, debug_color)
+	var position: Vector3 = (
+		global_position
+		+ (start as Vector3 * grid_scale).rotated(rotation_axis, rotation.get_angle())
+	)
+	DebugDraw3D.draw_box(position, rotation, (end - start) as Vector3 * grid_scale, debug_color)
--- src/shared/buildings/building_generation/building_shape_description.gd
+++ src/shared/buildings/building_generation/building_shape_description.gd
@@ -5,12 +5,18 @@ extends Node
 @export var is_visible: bool = false
 @export var gridmap: GridMap
 
+
 func get_cells() -> Array[Cell]:
 	var cells: Array[Cell]
 	cells.assign(find_children("", "BoxShape", false, true).map(func(box): return box.to_cell()))
 	return cells
 
+
 func _process(_delta: float) -> void:
 	if is_visible:
 		for box in find_children("", "BoxShape", false, true):
-			box.draw_visualization(gridmap.global_position, Quaternion.from_euler(gridmap.global_rotation), gridmap.cell_size)
+			box.draw_visualization(
+				gridmap.global_position,
+				Quaternion.from_euler(gridmap.global_rotation),
+				gridmap.cell_size
+			)

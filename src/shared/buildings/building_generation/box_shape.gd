b78cf3dc19be2472e48650e817e7af16e3f63c10
ThreeCoffes <leonard.madzielewski@gmail.com>
2026-02-05 19:07:21 +0100

    add visualization

--- src/shared/buildings/building_generation/box_shape.gd
+++ src/shared/buildings/building_generation/box_shape.gd
@@ -6,14 +6,13 @@ extends Node
 @export var start: Vector3i
 @export var end: Vector3i
 
-@export_group("Visualization")
-@export var is_visible: bool = false
-
 @export_group("Other")
-@export var grid_scale: Vector3 = Vector3(2, 2.8, 2)
+@export var debug_color: Color
 
 func to_cell() -> Cell:
 	return Cell.new(start, end)
 
-func _draw() -> void:
-	pass
+func draw_visualization(global_position: Vector3, rotation: Quaternion, grid_scale: Vector3) -> void:
+	var rotation_axis = rotation.get_axis() if rotation.get_angle() != 0 else Vector3.UP
+	var position: Vector3 = global_position + (start as Vector3 * grid_scale).rotated(rotation_axis, rotation.get_angle()) 
+	DebugDraw3D.draw_box(position, rotation, (end-start) as Vector3 * grid_scale, debug_color)
--- src/shared/buildings/building_generation/building_shape_description.gd
+++ src/shared/buildings/building_generation/building_shape_description.gd
@@ -2,7 +2,15 @@
 class_name BuildingShapeDescription
 extends Node
 
+@export var is_visible: bool = false
+@export var gridmap: GridMap
+
 func get_cells() -> Array[Cell]:
 	var cells: Array[Cell]
-	cells.assign(find_children("", "BoxShape", true, true).map(func(box): return box.to_cell()))
+	cells.assign(find_children("", "BoxShape", false, true).map(func(box): return box.to_cell()))
 	return cells
+
+func _process(_delta: float) -> void:
+	if is_visible:
+		for box in find_children("", "BoxShape", false, true):
+			box.draw_visualization(gridmap.global_position, Quaternion.from_euler(gridmap.global_rotation), gridmap.cell_size)

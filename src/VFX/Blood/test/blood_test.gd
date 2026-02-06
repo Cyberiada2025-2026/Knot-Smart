9bbe39c47ddcdabe53a0619c958f9508c0eb5486
PawelSzczepan <szczeppawel@gmail.com>
2026-02-06 18:34:42 +0100

    review changes

--- src/VFX/Blood/test/blood_test.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-@export var particles: CPUParticles3D
-
-func _input(event):
-	if event is InputEventKey and event.pressed:
-		if event.keycode == KEY_SPACE:
-			particles.emitting = true
---
+++ src/shared/post_processing/camera_setup.gd
@@ -0,0 +1,7 @@
+extends Node
+class_name Camera_Setup
+
+@export var main_camera: Camera3D
+
+func get_main_camera():
+	return main_camera

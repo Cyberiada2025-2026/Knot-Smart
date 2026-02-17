f19a1772a58b81a20257d030ad67771c2d6d0c68
Kurak <dawid.djpk8@gmail.com>
2026-02-17 21:24:36 +0100

    now makes first footstep after pressing key

--- src/characters/player/player_detection/footstapes_maker.gd
+++ src/characters/player/player_detection/footstapes_maker.gd
@@ -13,6 +13,7 @@ func make_footstep():
 
 func _physics_process(_delta: float) -> void:
 	if player_physics.velocity != Vector3.ZERO && player_physics.is_on_floor() && timer.is_stopped():
+		make_footstep()
 		timer.start()
 
 

e22e5d929ca5d9c78a9ed0052ff2770b40a078db
alanva36 <alanva36official@gmail.com>
2026-02-25 21:15:00 +0100

    updated documentation

--- src/shared/terrain/world_generation_params.gd
+++ src/shared/terrain/world_generation_params.gd
@@ -2,17 +2,22 @@ class_name WorldGenerationParams
 extends Resource
 
 ## The total width and depth of the map in world units.
-@export_range(4, 2048, 4) var map_size := 64
+## Unit: [b]int[/b] (Chunks).
+@export_range(4, 2048, 4) var map_size := 8
 
+## Unit: [b]float[/b] (Godot units / meters).
 @export_range(4, 2048, 4.0) var map_height := 64.0
 
 ## The size of an individual data chunk. Smaller chunks allow for 
 ## more granular optimization but increase draw calls.
+## Unit: [b]int[/b] (Tiles).
 @export_range(4, 32, 4) var chunk_size := 4
 
 ## The horizontal dimensions of a single tile or face within the mesh.
+## Unit: [b]int[/b] (Godot units / meters).
 @export_range(1, 100, 1) var tile_size := 8
 
+## Unit: [b]float[/b] (Godot units / meters).
 @export_range(1, 100, 0.5) var tile_height := 8.0
 
 ## The noise algorithm used to calculate elevation.

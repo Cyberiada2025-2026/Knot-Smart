a3cd1011f6836c6d39507a229e7774919866e729
PawelSzczepan <szczeppawel@gmail.com>
2026-02-27 17:04:04 +0100

    linter4

--- src/shared/camera/camera_holder.gd
+++ src/shared/camera/camera_holder.gd
@@ -1,6 +1,7 @@
 extends Node
 
-var camera_setup = load(ResourceUID.get_id_path(ResourceUID.text_to_id("uid://rnx3yu0ofiwg")))
+var id: int = ResourceUID.text_to_id("uid://rnx3yu0ofiwg")
+var camera_setup = load(ResourceUID.get_id_path(id))
 var instance: Node3D
 var main_camera: Camera3D
 

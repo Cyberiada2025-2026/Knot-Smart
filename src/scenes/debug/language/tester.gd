8467c1edb3e8b8162d418a235a615482ded2db63
Kurak <dawid.djpk8@gmail.com>
2026-03-06 14:44:08 +0100

    fixed my hallucynations

--- src/scenes/debug/language/tester.gd
+++ src/scenes/debug/language/tester.gd
@@ -1,6 +1,7 @@
 extends Node
 
 @export var language_generation : LanguageGeneration
+@export var mood : AlienMoods.Moods
 var input : String = "Litwo! Ojczyzno moja! ty jesteś jak zdrowie.
 Ile cię trzeba cenić, ten tylko się dowie,
 Kto cię stracił. Dziś piękność twą w całej ozdobie
@@ -30,4 +31,4 @@ Zieloną, na niej z rzadka ciche grusze siedzą."
 func _ready() -> void:
 	var out = language_generation.process_dialogue(input)
 	#print(out)
-	speech_player.play_speech(out)
+	speech_player.play_speech(out, mood)
--- src/shared/sfx/alien_speech/speech_player.gd
+++ src/shared/sfx/alien_speech/speech_player.gd
@@ -6,20 +6,19 @@ var curr_sentence = []
 @onready var speech_types: Node = $"../SpeechTypes"
 
 
-func play_speech(input: Array) -> void:
+func play_speech(input: Array, mood : AlienMoods.Moods = AlienMoods.Moods.neutral) -> void:
 	for sentence in input:
 		for word in sentence:
 			for i in range(len(word)):
 				if i != len(word)-1:
-					play_sound(word[i])
+					play_sound(word[i], true, mood)
 				else:
-					play_sound(word[i], false)
+					play_sound(word[i], false, mood)
 				await audio_stream_player.finished
 		await get_tree().create_timer(0.75).timeout
 
 
 func play_sound(sound: String, cut_ending: bool = true, mood : AlienMoods.Moods = AlienMoods.Moods.neutral) -> void:
-	print()
 	var speech : Speech = speech_types.get_node(AlienMoods.get_mood_name(mood))
 	var sound_file : AudioStream = speech.speech_type.sounds[sound]
 	audio_stream_player.stream = sound_file

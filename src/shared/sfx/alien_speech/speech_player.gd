d2588a256b98a9dd4229919bb26bea33bc859f97
Kurak <dawid.djpk8@gmail.com>
2026-03-07 14:59:01 +0100

    gdlint test

--- src/shared/sfx/alien_speech/speech_player.gd
+++ src/shared/sfx/alien_speech/speech_player.gd
@@ -6,11 +6,11 @@ var curr_sentence = []
 @onready var speech_types: Node = $"../SpeechTypes"
 
 
-func play_speech(input: Array, mood : AlienMoods.Moods = AlienMoods.Moods.neutral) -> void:
+func play_speech(input: Array, mood: AlienMoods.Moods = AlienMoods.Moods.neutral) -> void:
 	for sentence in input:
 		for word in sentence:
 			for i in range(len(word)):
-				if i != len(word)-1:
+				if i != len(word) - 1:
 					play_sound(word[i], true, mood)
 				else:
 					play_sound(word[i], false, mood)
@@ -18,16 +18,18 @@ func play_speech(input: Array, mood : AlienMoods.Moods = AlienMoods.Moods.neutra
 		await get_tree().create_timer(0.75).timeout
 
 
-func play_sound(sound: String, cut_ending: bool = true, mood : AlienMoods.Moods = AlienMoods.Moods.neutral) -> void:
-	var speech : Speech = speech_types.get_node(AlienMoods.get_mood_name(mood))
-	var sound_file : AudioStream = speech.speech_type.sounds[sound]
+func play_sound(
+	sound: String, cut_ending: bool = true, mood: AlienMoods.Moods = AlienMoods.Moods.neutral
+) -> void:
+	var speech: Speech = speech_types.get_node(AlienMoods.get_mood_name(mood))
+	var sound_file: AudioStream = speech.speech_type.sounds[sound]
 	audio_stream_player.stream = sound_file
 	audio_stream_player.pitch_scale = 1.1 + randf_range(-0.05, 0.05)
 	audio_stream_player.play()
-	
+
 	if cut_ending:
-		var play_duration = sound_file.get_length() * 0.9
-		
+		var play_duration := sound_file.get_length() * 0.9
+
 		await get_tree().create_timer(play_duration).timeout
 		audio_stream_player.stop()
 		audio_stream_player.emit_signal("finished")

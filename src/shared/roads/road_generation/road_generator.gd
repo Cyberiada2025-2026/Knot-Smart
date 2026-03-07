2bc7a516ba8ffdfb17e1bcbde36cfe8a35796701
Titanic472 <ctac472@gmail.com>
2026-03-07 21:06:29 +0100

    gdlint checks

--- src/shared/roads/road_generation/road_generator.gd
+++ src/shared/roads/road_generation/road_generator.gd
@@ -11,20 +11,24 @@ extends Node
 @export var map_visualization: bool
 
 @export var log_generation_steps: bool
-@export_tool_button("Log Generated Map") var log_generated_map_to_console = _print_to_console.bind("type")
+@export_tool_button("Log Generated Map") var log_generated_map_to_console = (
+	_print_to_console.bind("type")
+)
 @export_tool_button("Log ID's") var log_id_to_console = _print_to_console.bind("id")
-@export_tool_button("Log Rotations") var log_rotations_to_console = _print_to_console.bind("rotation")
+@export_tool_button("Log Rotations") var log_rotations_to_console = (
+	_print_to_console.bind("rotation")
+)
 
-var _spot_generator = SpotGenerator.new()
 var blueprint: Dictionary
+var _spot_generator = SpotGenerator.new()
 var _map_size: int
-	
+
 func _process(_delta: float) -> void:
 	if limit_areas_visualization:
 		_visualize_limiter_areas()
 	if map_visualization:
 		_visualize()
-		
+
 
 ## Generate basic road map [br][br]
 ## Add Limiter Areas to "GenerationAreas" node to customize generation [br][br]
@@ -34,33 +38,33 @@ func _process(_delta: float) -> void:
 func generate_roads(_blueprint: Dictionary) -> bool:
 	if log_generation_steps:
 		print("start road generation!")
-	
+
 	_map_size = generation_params.map_size
-	
+
 	if _blueprint.is_empty():
 		# will be replaced by default creation from blueprint class
 		_create_default_blueprint()
 	else:
-		blueprint = _blueprint	
-	
+		blueprint = _blueprint
+
 	generation_params.generation_areas = _get_generation_areas()
 	generation_params.prepare_generation_areas()
-	
+
 	_spot_generator.generate_spots(self)
-	
+
 	_spot_generator.cast_spots_to_blueprint()
-	
+
 	# later will be splitted and used outside of road generator
 	var autotiler: RoadAutotile = RoadAutotile.new()
-	
+
 	if not autotiler.autotile_roads(blueprint, _map_size):
 		return false
-		
+
 	if log_generation_steps:
 		print("finished full generation!\n")
-		
+
 	return true
-	
+
 
 func _create_generation_areas():
 	var generation_areas = GenerationAreas.new()
@@ -68,15 +72,15 @@ func _create_generation_areas():
 	self.add_child(generation_areas)
 	generation_areas.owner = get_tree().edited_scene_root
 	return generation_areas
-	
+
 
 func _get_generation_areas() -> Array[LimiterArea]:
 	var generation_areas = find_child("GenerationAreas", false)
-	
+
 	if generation_areas == null:
 		print("Generation areas not found, adding empty")
 		generation_areas = _create_generation_areas()
-		
+
 	return generation_areas.get_limiter_areas()
 
 
@@ -101,7 +105,7 @@ func _print_to_console(key: String) -> void:
 	if blueprint.is_empty():
 		return
 	print("printing '", key, "':")
-		
+
 	for y in _map_size:
 		var output: String = ""
 		for x in _map_size:
@@ -118,26 +122,26 @@ func _print_to_console(key: String) -> void:
 			else:
 				output += "  "
 		print(output)
-		
+
 
 ## Simple test visualization
 func _visualize() -> void:
 	# visualize spots
 	for spot in _spot_generator.get_spots():
 		spot.visualize()
-	
+
 	# visualize roads
 	for coord in blueprint.keys():
 		if blueprint[coord]["type"] == "road":
 			DebugDraw3D.draw_box(
 				Vector3(coord.x, 0, coord.y),
-					Quaternion.IDENTITY, 
+					Quaternion.IDENTITY,
 					Vector3(1, 0.01, 1),
 					Color(1.0, 1.0, 1.0, 1.0),
 					false
 				)
-				
-				
+
+
 ## visualization for better areas setup
 func _visualize_limiter_areas():
 	for area in _get_generation_areas():
--- src/shared/roads/road_generation/spot_generator.gd
+++ src/shared/roads/road_generation/spot_generator.gd
@@ -6,7 +6,7 @@ var _blueprint: Dictionary
 var _map_size: int
 var _final_spots: Array[Spot] = []
 
-	
+
 func generate_spots(road_generator: RoadGenerator):
 	_blueprint = road_generator.blueprint
 	var generation_params = road_generator.generation_params
@@ -14,46 +14,46 @@ func generate_spots(road_generator: RoadGenerator):
 	var spots: Array[Spot] = []
 	# clear previous generation results
 	_final_spots.clear()
-	
+
 	# create initial rectangle spot that will be divided into smaller ones
 	spots.push_back(Spot.new(Vector2i(0, 0), Vector2i(_map_size - 1, _map_size - 1)))
-	
+
 	var steps_success: int = 0
 	var steps_all: int = 0
-	
+
 	# splitting rectangles until they reach proper size
 	while not spots.is_empty() and steps_all < generation_params.generation_steps_limit:
 		steps_all += 1
 		var curr_spot_idx: int = randi_range(0, len(spots) - 1)
 		var curr_spot: Spot = spots[curr_spot_idx]
-		
+
 		var area = generation_params.get_area(curr_spot)
-			
+
 		# action decides whether we are splitting x or y direction
 		var axis = Utils.Axis2.values().pick_random()
-		
+
 		if _split_spot(curr_spot, area, axis, spots):
 			steps_success += 1
-			
+
 		if _is_spot_correctly_sized(curr_spot, area.max_spot_size):
 			var spot = spots.pop_at(curr_spot_idx)
 			if not _is_spot_touching_map_bounds(spot):
 				_final_spots.push_back(spot)
-			
+
 		# highways are created by moving all spots's start by 1
 		if steps_success == generation_params.highway_generation_split_count:
 			_move_spot_start(spots)
 			_move_spot_start(_final_spots)
 			steps_success += 1
-	
+
 	if not spots.is_empty():
 		if road_generator.log_generation_steps:
 			print("full splits failed for ", spots.size(), " spots, moving them to final array")
 		_final_spots.append_array(spots.filter(_is_spot_touching_map_bounds))
-	
+
 	if road_generator.log_generation_steps:
 		print("Roads generated, road generation success steps: ", steps_success, " all: ", steps_all)
-		
+
 
 func cast_spots_to_blueprint():
 	for spot in _final_spots:
@@ -64,8 +64,8 @@ func cast_spots_to_blueprint():
 
 func get_spots():
 	return _final_spots
-	
-	
+
+
 ## Get coordinates of all points located between start and end positions
 func _get_area_positions_array(start: Vector2i, end: Vector2i) -> Array:
 	var coordinates: Array[Vector2i]
@@ -73,7 +73,7 @@ func _get_area_positions_array(start: Vector2i, end: Vector2i) -> Array:
 		for y in range(start.y, end.y + 1):
 			coordinates.push_back(Vector2i(x, y))
 	return coordinates
-	
+
 
 func _is_valid_tile(position: Vector2i, axis: int):
 	return (
@@ -87,54 +87,54 @@ func _is_valid_tile(position: Vector2i, axis: int):
 			)
 			or _blueprint[position]["can_place"] == "any"
 		)
-	
-	
+
+
 ## Splits spot into 2 smaller ones if possible
 func _split_spot(spot: Spot, area: LimiterArea, axis: int, spots: Array) -> bool:
 	if spot.size()[axis] <= area.max_spot_size[axis]:
 		return false
-		
+
 	var split_point = randi_range(
 		area.min_spot_size[axis],
 		spot.size()[axis] - area.min_spot_size[axis]
 	)
-	
+
 	var e1: Vector2i = spot.end
 	var s2: Vector2i = spot.start
 	e1[axis] = spot.start[axis] + split_point
 	s2[axis] = spot.start[axis] + split_point
-	
+
 	# avoid placing streets on incorrect slopes and terrain corners
 	for position in _get_area_positions_array(s2, e1):
 		if not _is_valid_tile(position, axis):
 			return false
-	
+
 	var new_spot: Spot = Spot.new(s2, spot.end)
 	spot.end = e1
 	spots.push_back(new_spot)
 	return true
-	
-	
+
+
 func _is_spot_correctly_sized(spot: Spot, max_spot_size: Vector2i) -> bool:
 	for axis in Utils.Axis2.values():
 		if spot.size()[axis] > max_spot_size[axis]:
 			return false
 	return true
-	
-	
+
+
 func _is_spot_touching_map_bounds(spot: Spot) -> bool:
 	for axis in Utils.Axis2.values():
 		if(
 			spot.start[axis] == 0
-			or spot.end[axis] ==  _map_size - 1 
+			or spot.end[axis] ==  _map_size - 1
 		):
 			return true
 	return false
-	
-	
+
+
 ## Move spots' start 1 tile forward
 func _move_spot_start(spots: Array[Spot]):
 	for axis in Utils.Axis2.values():
 		for spot in spots:
 			if spot.start[axis] != 0:
-				spot.start[axis] += 1	
+				spot.start[axis] += 1

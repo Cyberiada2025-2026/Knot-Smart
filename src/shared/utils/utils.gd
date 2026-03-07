a1f2133227d82664cbfa7a2d39e20a0d43556ccc
sajmon <szymon17099@gmail.com>
2026-03-07 23:05:19 +0100

    Add raycast utilities

--- src/shared/utils/utils.gd
+++ src/shared/utils/utils.gd
@@ -7,3 +7,26 @@ enum Axis {
 	Y = Vector3i.Axis.AXIS_Y,
 	Z = Vector3i.Axis.AXIS_Z,
 }
+
+
+## Unsafe: Has to be called from within _physics_process.
+## Performs a ray-cast from the position on ctx's viewport.
+static func unsafe_raycast_from_screen_pos(ctx: Node3D, position: Vector2, ray_length = 1000.0):
+	var camera = CameraSingleton.get_main_camera()
+	var from = camera.project_ray_origin(position)
+	var normal = camera.project_ray_normal(position)
+	var to = from + normal * ray_length
+	
+	var space_state = ctx.get_world_3d().direct_space_state
+	var query = PhysicsRayQueryParameters3D.create(from, to)
+	var result = space_state.intersect_ray(query)
+
+	return result
+
+
+## Unsafe: Has to be called from within _physics_process.
+## Performs a ray-cast from the center of ctx's viewport
+static func unsafe_raycast_from_screen_center(ctx: Node3D, ray_length = 1000.0):
+	var camera = CameraSingleton.get_main_camera()
+	var center_pos = camera.get_viewport().size_2d_override / 2
+	return unsafe_raycast_from_screen_pos(ctx, center_pos, ray_length)

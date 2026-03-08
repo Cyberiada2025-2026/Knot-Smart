163a639d0dc4ce990ea324d0a82612c4d35d15fd
Thedoczek <dokmiada@gmail.com>
2026-03-08 17:25:30 +0100

    Lint and format checks

--- src/shared/language_generation/language_generation.gd
+++ src/shared/language_generation/language_generation.gd
@@ -6,10 +6,11 @@ static var constants = preload("res://shared/language_generation/language_consta
 static var regex_sentence = RegEx.create_from_string("(?:(?:\\p{Z}|\\p{P})?[\\p{L}\\p{N}\\p{S}])+")
 static var regex_word = RegEx.create_from_string("\\p{L}+")
 
+
 func preprocess_string(input_str: String):
 	var output = []
 	input_str = input_str.to_lower()
-	
+
 	for sentence in regex_sentence.search_all(input_str):
 		var sentence_output = []
 		for word in regex_word.search_all(sentence.get_string()):
@@ -17,10 +18,11 @@ func preprocess_string(input_str: String):
 		output.push_back(sentence_output)
 
 	return output
-	
+
+
 func translate_word(word: String):
-	if word in constants.PRESETS:
-		return(constants.PRESETS[word])
+	if word in constants.presets:
+		return constants.presets[word]
 
 	while word.length() < 2:
 		word += " "
@@ -28,20 +30,25 @@ func translate_word(word: String):
 	var height_map = []
 	for i in range(word.length() - 1):
 		# iterating through pairs of letters and checking the noise value for that pair
-		height_map.append(FastNoiseLite.new().get_noise_2d(word.unicode_at(i), word.unicode_at(i + 1)))
+		height_map.append(
+			FastNoiseLite.new().get_noise_2d(word.unicode_at(i), word.unicode_at(i + 1))
+		)
 	var output_word = []
 
 	for i in range(height_map.size()):
-		if (i > 0 and height_map[i] <= height_map[i - 1]) \
-		or (i < height_map.size() - 1 and height_map[i] <= height_map[i + 1]):
+		if (
+			(i > 0 and height_map[i] <= height_map[i - 1])
+			or (i < height_map.size() - 1 and height_map[i] <= height_map[i + 1])
+		):
 			continue
 		# the number must be bigger than the neighbours in order to generate a syllable
 		# in other words, the height must be a local peak
 
-		var result_syllable_id = abs(hash(height_map[i])) % len(constants.SYLLABLES)
-		output_word.push_back(constants.SYLLABLES[result_syllable_id])
-	
+		var result_syllable_id = abs(hash(height_map[i])) % len(constants.syllables)
+		output_word.push_back(constants.syllables[result_syllable_id])
+
 	return output_word
+
 
 func translate_array(input_array: Array):
 	var output_line_array = []
@@ -66,7 +73,7 @@ func process_dialogue(input_str):
 
 func _ready() -> void:
 	print(constants)
-	
+
 	var dialogue = "This can't be... there's no way that's true! Mr. Smith is dead!"
 	print(dialogue)
 

21bc6395feff2ee6c8dedcce5b5283f4430bc87a
Thedoczek <dokmiada@gmail.com>
2026-03-08 17:17:02 +0100

    Code cleanup
    - Class has been registered
    - Constants have been moved to a JSON outside

--- src/shared/language_generation/language_generation.gd
+++ src/shared/language_generation/language_generation.gd
@@ -1,21 +1,15 @@
+class_name LanguageGenerator
 extends Node
 
-# we need to move these consts out
-const SYLLABLES = [
-	"zip", "ba", "rim", "waeb", "ga", "womp", "go", "lip", "blop", "zop", "lle", "si"
-]
-const PRESETS = {"kurwa": ["waeb", "ba"], "mać": ["zip"], "do": ["zip"], "widzenia": ["zop"]}
-var syllable_count = len(SYLLABLES)
-var noise = FastNoiseLite.new()
+static var constants = preload("res://shared/language_generation/language_constants.json").data
 
-var regex_sentence = RegEx.create_from_string("(?:(?:\\p{Z}|\\p{P})?[\\p{L}\\p{N}\\p{S}])+")
-var regex_word = RegEx.create_from_string("\\p{L}+")
-
+static var regex_sentence = RegEx.create_from_string("(?:(?:\\p{Z}|\\p{P})?[\\p{L}\\p{N}\\p{S}])+")
+static var regex_word = RegEx.create_from_string("\\p{L}+")
 
 func preprocess_string(input_str: String):
 	var output = []
 	input_str = input_str.to_lower()
-
+	
 	for sentence in regex_sentence.search_all(input_str):
 		var sentence_output = []
 		for word in regex_word.search_all(sentence.get_string()):
@@ -23,7 +17,31 @@ func preprocess_string(input_str: String):
 		output.push_back(sentence_output)
 
 	return output
+	
+func translate_word(word: String):
+	if word in constants.PRESETS:
+		return(constants.PRESETS[word])
 
+	while word.length() < 2:
+		word += " "
+
+	var height_map = []
+	for i in range(word.length() - 1):
+		# iterating through pairs of letters and checking the noise value for that pair
+		height_map.append(FastNoiseLite.new().get_noise_2d(word.unicode_at(i), word.unicode_at(i + 1)))
+	var output_word = []
+
+	for i in range(height_map.size()):
+		if (i > 0 and height_map[i] <= height_map[i - 1]) \
+		or (i < height_map.size() - 1 and height_map[i] <= height_map[i + 1]):
+			continue
+		# the number must be bigger than the neighbours in order to generate a syllable
+		# in other words, the height must be a local peak
+
+		var result_syllable_id = abs(hash(height_map[i])) % len(constants.SYLLABLES)
+		output_word.push_back(constants.SYLLABLES[result_syllable_id])
+	
+	return output_word
 
 func translate_array(input_array: Array):
 	var output_line_array = []
@@ -32,33 +50,7 @@ func translate_array(input_array: Array):
 		var output_word_array = []
 
 		for word in sentence:
-			if word in PRESETS:
-				output_word_array.push_back(PRESETS[word])
-				continue
-
-			while word.length() < 2:
-				word += " "
-
-			var height_map = []
-			for i in range(word.length() - 1):
-				# iterating through pairs of letters and checking the noise value for that pair
-				height_map.append(noise.get_noise_2d(word.unicode_at(i), word.unicode_at(i + 1)))
-
-			var output_word = []
-
-			for i in range(height_map.size()):
-				if i > 0 and height_map[i] <= height_map[i - 1]:
-					continue
-				if i < height_map.size() - 1 and height_map[i] <= height_map[i + 1]:
-					continue
-				# the number must be bigger than the neighbours in order to generate a syllable
-				# in other words, the height must be a local peak
-
-				var result_syllable_id = abs(hash(height_map[i])) % syllable_count
-				output_word.push_back(SYLLABLES[result_syllable_id])
-
-			# joining SYLLABLES into word
-			output_word_array.push_back(output_word)
+			output_word_array.push_back(translate_word(word))
 
 		# joining words into a sentence
 		output_line_array.push_back(output_word_array)
@@ -73,4 +65,11 @@ func process_dialogue(input_str):
 
 
 func _ready() -> void:
-	pass
+	print(constants)
+	
+	var dialogue = "This can't be... there's no way that's true! Mr. Smith is dead!"
+	print(dialogue)
+
+	var translation = process_dialogue(dialogue)
+	for sentence in translation:
+		print(sentence)

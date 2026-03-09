7187b983232a681c38f3414add1551c0f150ab0e
sajmon <szymon17099@gmail.com>
2026-03-09 02:54:34 +0100

    Separate equilibrium strategies into files

---
+++ src/characters/player/rope/strategies/basic_static_strategy.gd
@@ -0,0 +1,12 @@
+class_name BasicStaticStrategy
+extends Node
+
+
+func get_strategy_type() -> RopeEnd.StrategyType:
+	return RopeEnd.StrategyType.STATIC
+
+func get_equilibrium(current: RopeEnd, _other: RopeEnd):
+	return current.position
+
+func release_force(_end: RopeEnd, _node: Node):
+	return

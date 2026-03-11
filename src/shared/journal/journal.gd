e1a519d59cb6404ecf4c52dfd48db85698fc7010
Kovilli <s203283@student.pg.edu.pl>
2026-03-11 18:57:11 +0100

    add focus on buttons

--- src/shared/journal/journal.gd
+++ src/shared/journal/journal.gd
@@ -1,14 +1,16 @@
 ## Manages the journal system
 extends Node
 
-var is_visible = false
+var is_visible: bool = false
 var menu: Node
 var pages: Node
+var button_normal
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	menu = get_child(0)
 	pages = menu.get_child(1)
+	button_normal = menu.get_child(0).get_child(1).get_theme_stylebox("normal", "Button")
 	pass # Replace with function body.
 
 func _init() -> void:
@@ -30,11 +32,15 @@ func _physics_process(delta: float) -> void:
 	pass
 
 func _on_button_pressed(number: int) -> void:
+	var button: Button = menu.get_child(0).get_child(number)
 	print(number+1)
 	
 	for i in range(pages.get_child_count()):
 		pages.get_child(i).set_visible(false)
-	
+		menu.get_child(0).get_child(i).add_theme_stylebox_override("normal",button_normal)
+		
 	pages.get_child(number).set_visible(true)
-
+	button.add_theme_stylebox_override("normal",button.get_theme_stylebox("press", "Button"))
+	
+	print(pages.get_child(number).has_focus())
 	pass # Replace with function body.

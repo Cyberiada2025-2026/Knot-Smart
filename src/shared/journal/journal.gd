10a0b6c01aa38dfaae8f00b3bd2f1eb3b71cd971
Kovilli <s203283@student.pg.edu.pl>
2026-03-11 15:59:25 +0100

    add button tab

--- src/shared/journal/journal.gd
+++ src/shared/journal/journal.gd
@@ -2,12 +2,12 @@
 extends Node
 
 var is_visible = false
-var button1: Node
+var menu: Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	button1 = get_child(0)
-	button1.set_visible(false)
+	menu = get_child(0)
+	menu.set_visible(false)
 	pass # Replace with function body.
 
 func _init() -> void:
@@ -18,17 +18,28 @@ func _process(delta: float) -> void:
 	
 	if Input.is_action_just_pressed("journal_show"):
 		print("click1")
-		if button1.visible == true:
-			button1.visible = false
+		if menu.visible == true:
+			menu.visible = false
 		else:
-			button1.visible = true
+			menu.visible = true
 	
 	pass
 	
 func _physics_process(delta: float) -> void:
 	pass
 
+func _on_button1_pressed() -> void:
+	print("1")
+	pass # Replace with function body.
 
-func _on_button_pressed() -> void:
-	print("click2")
+func _on_button2_pressed() -> void:
+	print("2")
+	pass # Replace with function body.
+
+func _on_button3_pressed() -> void:
+	print("3")
+	pass # Replace with function body.
+
+func _on_button4_pressed() -> void:
+	print("4")
 	pass # Replace with function body.

b65bc1b022aeadc0a886520b6c27e94c8978667a
ThreeCoffes <leonard.madzielewski@gmail.com>
2026-03-11 20:29:01 +0100

    switch to a different user interface wip

--- src/shared/day_night_cycle/day_night_cycle.gd
+++ src/shared/day_night_cycle/day_night_cycle.gd
@@ -5,23 +5,31 @@ extends Node3D
 ## Duration in seconds from beginning of day
 @export var initial_time: float = 0.0
 
-@onready var environment: Environment = get_viewport().get_camera_3d().environment
+@export var environment: Environment
 
 var day_duration: float 
 
 var curr_day: int = 0
 
-signal time_of_day_started(name: String)
-signal day_ended(curr_day: int)
+signal time_of_day_started(current: TimeOfDay)
+signal new_day_started(curr_day: int)
 
 func start():
-	for tod in times_of_day:
-		await SceneTree.create_timer(tod.duration)
+	while true:
+		for tod in times_of_day:
+			time_of_day_started.emit(tod.name)
+			environment.adjustment_color_correction = tod.color_lut
+			print(tod.name, " started")
+			await get_tree().create_timer(tod.duration).timeout
+
+		curr_day += 1
+		new_day_started.emit(curr_day)
+		print("Day ", curr_day, " started")
 
 func _ready() -> void:
 	environment.adjustment_enabled = true
 
 	day_duration = times_of_day.reduce(func(a: float, t: TimeOfDay): return a + t.duration, 0.0) 
-
-	curr_time = initial_time
-
+	
+	if not Engine.is_editor_hint():
+		start()

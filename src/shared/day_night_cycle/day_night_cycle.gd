ed85943ce6efd0e2324553fe760f1d45177c9a54
ThreeCoffes <leonard.madzielewski@gmail.com>
2026-03-11 22:34:06 +0100

    add own timer logic

--- src/shared/day_night_cycle/day_night_cycle.gd
+++ src/shared/day_night_cycle/day_night_cycle.gd
@@ -1,35 +1,55 @@
 @tool
 extends Node3D
 
-@export var times_of_day: Array[TimeOfDay] = []
-## Duration in seconds from beginning of day
-@export var initial_time: float = 0.0
+## Times of day that constitute one cycle
+@export var times_of_day: Array[TimeOfDay] = []:
+	set(value):
+		times_of_day = value
+		day_duration = times_of_day.reduce(func(a: float, t: TimeOfDay): return a + t.duration, 0.0) 
+
+## Duration in seconds from beginning of day zero
+@export var seconds_since_start: float = 0.0:
+	set(value):
+		var last_tod: TimeOfDay = get_current_time_of_day()
+		var last_day: int = get_current_day()
+
+		seconds_since_start = value
+
+		var curr_tod: TimeOfDay = get_current_time_of_day()
+		var curr_day: int = get_current_day()
+
+		if curr_tod != last_tod:
+			time_of_day_started.emit(curr_tod)
+		if curr_day != last_day:
+			new_day_started.emit(curr_day)
 
 @export var environment: Environment
 
 var day_duration: float 
 
-var curr_day: int = 0
+
+func get_current_day() -> int:
+	return floor(seconds_since_start / day_duration)
+
+func get_current_time() -> float:
+	return fmod(seconds_since_start, day_duration)
+
+func get_current_time_of_day() -> TimeOfDay:
+	var time = get_current_time()
+	for tod in times_of_day:
+		if time <= 0:
+			return tod
+		time -= tod.duration
+	return times_of_day[-1]
+		
 
 signal time_of_day_started(current: TimeOfDay)
 signal new_day_started(curr_day: int)
 
-func start():
-	while true:
-		for tod in times_of_day:
-			time_of_day_started.emit(tod.name)
-			environment.adjustment_color_correction = tod.color_lut
-			print(tod.name, " started")
-			await get_tree().create_timer(tod.duration).timeout
-
-		curr_day += 1
-		new_day_started.emit(curr_day)
-		print("Day ", curr_day, " started")
+func _physics_process(delta: float) -> void:
+	if not Engine.is_editor_hint():
+		seconds_since_start += delta
 
 func _ready() -> void:
 	environment.adjustment_enabled = true
 
-	day_duration = times_of_day.reduce(func(a: float, t: TimeOfDay): return a + t.duration, 0.0) 
-	
-	if not Engine.is_editor_hint():
-		start()

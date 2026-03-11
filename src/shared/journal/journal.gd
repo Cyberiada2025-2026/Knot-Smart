f2bed5177615007e36fc52977698db05d55bfd41
Kovilli <s203283@student.pg.edu.pl>
2026-03-11 15:12:53 +0100

    add test button

--- src/shared/journal/journal.gd
+++ src/shared/journal/journal.gd
@@ -1,8 +1,13 @@
 ## Manages the journal system
 extends Node
 
+var is_visible = false
+var button1: Node
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	button1 = get_child(0)
+	button1.set_visible(false)
 	pass # Replace with function body.
 
 func _init() -> void:
@@ -10,7 +15,20 @@ func _init() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	
+	if Input.is_action_just_pressed("journal_show"):
+		print("click1")
+		if button1.visible == true:
+			button1.visible = false
+		else:
+			button1.visible = true
+	
 	pass
 	
 func _physics_process(delta: float) -> void:
 	pass
+
+
+func _on_button_pressed() -> void:
+	print("click2")
+	pass # Replace with function body.

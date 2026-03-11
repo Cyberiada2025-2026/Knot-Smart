c8a58ae323e577e4cdb9f8a444a7041b1c7873c6
ThreeCoffes <leonard.madzielewski@gmail.com>
2026-03-11 17:10:24 +0100

    wip

--- src/shared/day_night_cycle/day_night_cycle.gd
+++ src/shared/day_night_cycle/day_night_cycle.gd
@@ -1,97 +1,39 @@
 @tool
 extends Node3D
 
-enum TimeOfDay {
-	DAWN,
-	DAY,
-	SUNSET,
-	NIGHT
-}
-
 @export_group("Cycle parameters")
-@export var day_length: float = 15 ## day length in real time minutes
-@export_range(0, 24) var dawn_start: float = 5.0 ## hour that dawn starts on. Dawn is considered night mechanics-wise
-@export_range(0, 24) var day_start: float = 6.0 ## hour that day starts on. Affects when the day_started signal is sent
-@export_range(0, 24) var sunset_start: float = 19.0 ## hour that sunset starts on. Sunset is considered day mechanics-wise
-@export_range(0, 24) var night_start: float = 20.0 ## hour that night starts on. Affects when the night_started signal is sent
-@export_range(0,24) var initial_hour: float = 7.0 ## hour that the cycle is started on. Affects the day counter and the initial sun postion.
-
-@export_group("Color correction")
-@export var LUT_day: Texture3D
-@export var LUT_sunset: Texture3D
-@export var LUT_night: Texture3D
+@export var times_of_day: Array[TimeOfDay] = []
+## Goes from beginning of the day to the end of the day.
+@export_range(0, 1) var initial_time: float = 0.0
 
 @export_group("Refs")
-@onready var world_environment: Environment = CameraSingleton.main_camera.environment
 @export var sun: DirectionalLight3D
-@export var day_timer: Timer
+@export var timer: Timer
+
+@onready var environment: Environment = get_viewport().get_camera_3d().environment
+var day_duration: float 
 
 var curr_day: int = 0
-var time_of_day: TimeOfDay:
+var curr_time_of_day: TimeOfDay:
 	set(value):
-		time_of_day = value
-		match value:
-			TimeOfDay.DAY:
-				world_environment.adjustment_color_correction = LUT_day
-				day_started.emit()
-				print("day started")
-			TimeOfDay.SUNSET:
-				world_environment.adjustment_color_correction = LUT_sunset
-				sunset_started.emit()
-				print("sunset started")
-			TimeOfDay.NIGHT:
-				world_environment.adjustment_color_correction = LUT_night
-				night_started.emit()
-				print("night started")
-			TimeOfDay.DAWN:
-				world_environment.adjustment_color_correction = LUT_sunset
-				dawn_started.emit()
-				print("dawn started")
+		curr_time_of_day = value
+		environment.adjustment_color_correction = value.color_lut
+		time_of_day_started.emit(value.name)
+		print("%s started" % value.name)
 
-signal dawn_started
-signal day_started
-signal sunset_started
-signal night_started
-
-func day_length_seconds() -> float:
-	return day_length * 60.0
-
-func hour_to_sun_rotation(hour: float) -> float:
-	var hour_normalized = hour/24
-	return hour_normalized * -2*PI + PI/2
-
-func time_to_hour(time: float) -> float:
-	return fmod((1 - time/day_length_seconds()) * 24 + initial_hour, 24)
+signal time_of_day_started(name: String)
 
 func _ready() -> void:
-	world_environment.adjustment_enabled = true
-	sun.rotation.x = hour_to_sun_rotation(initial_hour)
-	time_of_day = hour_to_time_of_day(initial_hour)
-	day_timer.wait_time = day_length_seconds()
-	if not Engine.is_editor_hint():
-		day_timer.start()
+	environment.adjustment_enabled = true
 
-func hour_to_time_of_day(hour: float) -> TimeOfDay:
-	if hour < dawn_start or hour >= night_start:
-		return TimeOfDay.NIGHT
-	if hour >= dawn_start and hour < day_start:
-		return TimeOfDay.DAWN
-	if hour >= day_start and hour < sunset_start:
-		return TimeOfDay.DAY
-	return TimeOfDay.SUNSET
+	day_duration = times_of_day.reduce(func(a: float, t: TimeOfDay): return a + t.duration, 0.0) 
+
+	timer.wait_time = day_duration
+
+	#time_of_day = hour_to_time_of_day(initial_hour)
+	#day_timer.wait_time = day_length_seconds()
+
 	
-func _process(_delta: float) -> void:
-	var curr_hour = time_to_hour(day_timer.time_left)
-
-	check_time_of_day_change(curr_hour)
-		
-	sun.rotation.x = hour_to_sun_rotation(curr_hour)
-
-func check_time_of_day_change(curr_hour: float):
-	var new_time_of_day = hour_to_time_of_day(curr_hour)
-	if new_time_of_day != time_of_day:
-		time_of_day = new_time_of_day
-
 
 func _on_day_timer_timeout() -> void:
 	curr_day+=1
---
+++ src/shared/day_night_cycle/time_of_day.gd
@@ -0,0 +1,8 @@
+@tool
+class_name TimeOfDay
+extends Resource
+
+@export var name: String
+@export var duration: float
+@export var color_lut: Texture3D
+@export var is_night: bool

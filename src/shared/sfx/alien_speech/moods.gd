4667087c6bdc8611a1c66f7a1cc59f189613c773
PlKurak <dawid.djpk8@gmail.com>
2026-03-12 22:04:45 +0100

    rebase

---
+++ src/shared/sfx/alien_speech/moods.gd
@@ -0,0 +1,13 @@
+class_name AlienMoods
+
+enum Moods {
+	angry,
+	zesty,
+	fast,
+	scared,
+	suprised,
+	neutral
+}
+
+static func get_mood_name(mood: Moods) -> String:
+	return Moods.keys()[mood]
---
+++ src/shared/sfx/alien_speech/speech_type_resource.gd
@@ -0,0 +1,18 @@
+extends Resource
+
+class_name SpeechType
+
+@export var sounds : Dictionary = {
+	"zip" = null, 
+	"ba" = null, 
+	"rim" = null,
+	"waeb" = null, 
+	"ga" = null, 
+	"womp" = null, 
+	"go" = null, 
+	"lip" = null, 
+	"blop" = null, 
+	"zop" = null,
+	"lle" = null, 
+	"si" = null
+}

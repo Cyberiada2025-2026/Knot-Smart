f2b746def1bf4561ca75ebd1f20f5fde4fa0adf6
sajmon <szymon17099@gmail.com>
2026-03-13 16:12:04 +0100

    Make collision radius bigger

--- src/characters/player/rope/rope_params.gd
+++ src/characters/player/rope/rope_params.gd
@@ -3,7 +3,7 @@ extends Resource
 
 @export var min_rope_length = 0.3
 @export var max_rope_length = 32.0
-@export var rope_collision_radius = 0.05
+@export var rope_collision_radius = 0.1
 
 @export var spring_constant = 7.5
 @export var damping = 0.5

42d73364421584ff0fd8f9b22ff4edd13bfbade9
Titanic472 <ctac472@gmail.com>
2026-03-13 14:12:59 +0100

    fixes

--- src/shared/point_of_interest/point_of_interest.gd
+++ src/shared/point_of_interest/point_of_interest.gd
@@ -1,53 +1,48 @@
 @tool
 class_name PointOfInterest
-extends Node
+extends Node3D
 
-
-@export var radius: float = 2:
+@export var radius: float = 0.5:
 	set(value):
 		radius = value
-		if collider is CollisionShape3D:
-			collider.shape.radius = radius
-		if visualization_mesh is MeshInstance3D:
-			visualization_mesh.mesh.radius = radius
-			visualization_mesh.mesh.height = radius * 2
+		collider.shape.radius = radius
+		visualization_mesh.mesh.radius = radius
+		visualization_mesh.mesh.height = radius * 2
 
 
 @export var visualize: bool = true:
 	set(value):
 		visualize = value
-		if visualization_mesh is MeshInstance3D:
-			visualization_mesh.visible = visualize
-
+		visualization_mesh.visible = visualize
 
 @export var trigger_group_name: StringName = "Player"
 
 ## message to say after triggering point of interest
 @export_multiline var message: String;
 
-## variable to avoiding doube-triggering
-var triggered: bool = false
-
-## scene to add if empty node is created in editor
-var scene = preload("res://shared/point_of_interest/point_of_interest.tscn")
+const VISUALIZATION_MATERIAL = preload("res://shared/point_of_interest/point_of_interest_visualization_material.tres")
 
 var collider
 var visualization_mesh
 
-func _ready():
-	# replace node with scene added to scene as node
-	if get_children().is_empty():
-		var scene_instance = scene.instantiate()
-		get_parent().add_child(scene_instance)
-		scene_instance.name = self.name
-		scene_instance.owner = get_parent()
-		self.queue_free()
-		return
-	collider = $Area3D/CollisionShape3D
-	visualization_mesh = $Area3D/MeshInstance3D
+
+func _init():
+	var area = Area3D.new()
+	collider = CollisionShape3D.new()
+	visualization_mesh = MeshInstance3D.new()
+
+	add_child(area)
+	area.body_entered.connect(_on_area_3d_body_entered)
+
+	area.add_child(collider)
+	collider.shape = SphereShape3D.new()
+
+	area.add_child(visualization_mesh)
+	visualization_mesh.mesh = SphereMesh.new()
+	visualization_mesh.mesh.material = VISUALIZATION_MATERIAL
+
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
-	if not triggered and body.get_parent().is_in_group(trigger_group_name):
-		triggered = true
+	if body.get_parent().is_in_group(trigger_group_name):
 		print(message)
-		visualization_mesh.visible = false;
+		self.queue_free()

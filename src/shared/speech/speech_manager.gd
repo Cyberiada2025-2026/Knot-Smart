5f865af9557f8fe2c06d9275b0bb7d014e90f506
PlKurak <dawid.djpk8@gmail.com>
2026-03-16 22:38:42 +0100

    refactored into resources

---
+++ src/shared/speech/speech_manager.gd
@@ -0,0 +1,40 @@
+class_name SpeechManager
+extends Node
+
+signal finished_playing
+
+var curr_sentence = []
+var voice_bank : VoiceBank
+
+var audio_stream_player: AudioStreamPlayer
+
+func _init() -> void:
+	audio_stream_player = AudioStreamPlayer.new()
+	add_child(audio_stream_player)
+
+
+func set_mood(mood: VoiceBank):
+	voice_bank = mood
+
+
+func play_sound(sound: String, multiplier: float = 1.0):
+	var sound_file: AudioStream = voice_bank.syllables[sound]
+	audio_stream_player.stream = sound_file
+	audio_stream_player.pitch_scale = voice_bank.base_pitch + randf_range(-voice_bank.pitch_variance, voice_bank.pitch_variance)
+	audio_stream_player.play()
+
+	var play_duration := sound_file.get_length() * multiplier
+	await get_tree().create_timer(play_duration).timeout
+	audio_stream_player.stop()
+	audio_stream_player.finished.emit()
+
+
+func play_speech(input: Array):
+	for sentence in input:
+		for word in sentence:
+			for i in range(len(word) - 1):
+				await play_sound(word[i], voice_bank.shorten_amount)
+			await play_sound(word.back())
+
+		await get_tree().create_timer(voice_bank.pause_between_sentence).timeout
+	finished_playing.emit()

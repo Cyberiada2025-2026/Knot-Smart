bb3bc2712e13be0f77b80ea5487bbe0175b883e3
ThreeCoffes <leonard.madzielewski@gmail.com>
2026-03-16 14:47:50 +0100

    add hitbox hurtbox and health component

---
+++ src/shared/health/hit_box.gd
@@ -0,0 +1,15 @@
+class_name HitBox
+extends Area3D
+
+@export var health_component: Node
+
+func _ready() -> void:
+	area_entered.connect(_on_area_entered)
+
+
+func _on_area_entered(area: Area3D) -> void:
+	var hurt_box = area as HurtBox
+	if hurt_box == null:
+		return
+
+	health_component.health -= hurt_box.damage
---
+++ src/shared/health/hurt_box.gd
@@ -0,0 +1,4 @@
+class_name HurtBox
+extends Area3D
+
+@export var damage: float = 1.0

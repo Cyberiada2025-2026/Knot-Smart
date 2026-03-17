e1eb2ed0b69b6bf6b97a70e84eff9bb6f283b739
Kovilli <s203283@student.pg.edu.pl>
2026-03-17 17:42:26 +0100

    style fix

--- src/shared/journal/journal.gd
+++ src/shared/journal/journal.gd
@@ -1,4 +1,3 @@
-## Manages the journal system
 extends Node
 
 var prev_mouse_mode
@@ -9,29 +8,30 @@ var is_visible: bool = false
 var menu: Control
 var pages: Control
 var button_normal
-var added_information: Array[bool] = [0, 0, 0, 0]
 var models: Array[Node3D]
 
-func add_object(description: String, name:String, model:Node3D, page_no: int):
+
+func add_object(description: String, name: String, model: Node3D, page_no: int, scale: float):
 	var page = pages.get_child(page_no).get_child(1).get_child(0)
 	var entry = entry_scene.instantiate()
-	page.add_child(entry);
+	page.add_child(entry)
 	var camera = entry.get_child(0).get_child(0).get_child(1)
-	model.set_scale(Vector3(0.3,0.3,0.3))
-	model.set_position(Vector3(0.0,0.0,-0.4))
+	model.set_scale(Vector3(scale, scale, scale))
+	model.set_position(Vector3(0.0, 0.0, -0.4))
 	camera.add_child(model)
 	models.append(model)
 	var obj_text = entry.get_child(1)
-	obj_text.append_text("[b]"+name+"[/b]\n"+description)
+	obj_text.append_text("[b]" + name + "[/b]\n" + description)
+
 
 func _ready() -> void:
 	menu = $"Journal menu"
 	pages = menu.get_child(1)
 	button_normal = menu.get_child(0).get_child(1).get_theme_stylebox("normal", "Button")
-	
+
+
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("journal_show"):
-		print("click1")
 		if not (get_tree().paused == true and menu.visible == false):
 			if menu.visible == true:
 				get_tree().paused = false
@@ -42,14 +42,13 @@ func _process(_delta: float) -> void:
 				menu.visible = true
 				prev_mouse_mode = Input.get_mouse_mode()
 				Input.set_mouse_mode(0)
-		
+
 	for model in models:
 		model.rotate_y(0.1)
 
 
 func _on_button_pressed(number: int) -> void:
 	var button: Button = menu.get_child(0).get_child(number)
-	print(number + 1)
 
 	if pages.get_child(number).visible == false:
 		for i in range(pages.get_child_count()):
--- src/shared/journal/object_description.gd
+++ src/shared/journal/object_description.gd
@@ -1,24 +1,27 @@
-class_name ObjectDescription 
+class_name ObjectDescription
 extends Node
 
 ##Insert model of an object
-@export var Model: Node3D
-##Insert name of an object
-@export var Name: String
+@export var model: Node3D
+##Type name of an object
+@export var object_name: String
 ##Insert description of the object
-@export var Description: String
-##Insert number at which page the object should be (3 - mobs, 2 - objects, 1 - items)
-@export var Page: int
+@export var description: String
+##Type at what page the object should be (3 - mobs, 2 - objects, 1 - items)
+@export var page: int
+##Set the scale of an object
+@export var scale: float
+
 
 func _init() -> void:
 	var poi: PointOfInterest = PointOfInterest.new()
 	poi.noticed.connect(on_object_notice)
-	poi.radius=1.0
+	poi.radius = 1.0
 	add_child(poi)
-	
+
+
 func on_object_notice() -> void:
 	var players = get_tree().get_nodes_in_group("Player")
 	var journal: Node = players[0].get_node("Journal")
-	print("weee")
-	var model_duplicate = Model.duplicate(7) 
-	journal.add_object(Description, Name, model_duplicate, Page)
+	var model_duplicate = model.duplicate(7)
+	journal.add_object(description, object_name, model_duplicate, page, scale)

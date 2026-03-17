951f7a6d53cf358e39b2e4267b0f071f4c7fc4c8
sajmon <szymon17099@gmail.com>
2026-03-17 12:29:19 +0100

    Fix infinite force accumulation

--- src/characters/player/rope/strategies/basic_kinematic_strategy.gd
+++ src/characters/player/rope/strategies/basic_kinematic_strategy.gd
@@ -11,6 +11,17 @@ func get_strategy_type() -> RopeEnd.StrategyType:
 
 
 func get_equilibrium(current: RopeEnd, other: RopeEnd):
+	var new_pull = pull
+
+	if Input.is_action_just_released("rope_axis_down"):
+		new_pull -= PULL_UNIT
+	elif Input.is_action_just_released("rope_axis_up"):
+		new_pull = clampf(new_pull + PULL_UNIT, new_pull, 0.0)
+	
+	if current.position.distance_squared_to(other.position) > 1.0 \
+		or abs(new_pull) < abs(pull):
+		pull = new_pull
+	
 	var unit_direction = (current.position - other.position).normalized()
 	return current.position - unit_direction * pull
 
@@ -18,12 +29,3 @@ func get_equilibrium(current: RopeEnd, other: RopeEnd):
 func release_force(end: RopeEnd, node: Node):
 	var direction = end.other.position - end.position
 	node.velocity += 2 * direction
-
-
-func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_released("rope_axis_down"):
-		print("Increased pull")
-		pull -= PULL_UNIT
-	elif event.is_action_released("rope_axis_up"):
-		print("Decreased pull")
-		pull += PULL_UNIT

23e0aca5d85d362197ad74cd0f73513d81cf16e8
Mikul-xyz <fmikulski@o2.pl>
2026-03-21 00:41:11 +0100

    linter

--- src/characters/player/gravity_controller.gd
+++ src/characters/player/gravity_controller.gd
@@ -14,17 +14,15 @@ var are_sensors_active: bool = true
 func get_floor_normal():
 	if not are_sensors_active:
 		return Vector3.UP
-	elif floor_sensor.is_colliding():
+	if floor_sensor.is_colliding():
 		return floor_sensor.get_collision_normal()
-	else:
-		return null
+	return null
 
 ## return detected wall normal or null otherwise
 func get_front_normal():
 	if are_sensors_active and front_sensor.is_colliding():
 		return front_sensor.get_collision_normal()
-	else:
-		return null
+	return null
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("turn_on-off_gravity_controller"):

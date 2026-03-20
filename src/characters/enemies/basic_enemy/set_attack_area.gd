aa819c27a8fbd5d168a6565e2e1a14d583aa1c04
ThreeCoffes <leonard.madzielewski@gmail.com>
2026-03-20 20:25:02 +0100

    fix charging enemy getting stuck on player bug

--- src/characters/enemies/basic_enemy/set_attack_area.gd
+++ src/characters/enemies/basic_enemy/set_attack_area.gd
@@ -4,7 +4,7 @@ extends ActionLeaf
 @export var area_disabled: bool = false
 @export var attack_area: TogglableArea
 
-func tick(actor: Node, blackboard: Blackboard) -> int:
+func tick(_actor: Node, _blackboard: Blackboard) -> int:
 	attack_area.set_disabled(area_disabled)
 
 	return SUCCESS

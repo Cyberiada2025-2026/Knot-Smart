2f83f3338874a4b585733a4cddbc6f819ba98187
ThreeCoffes <leonard.madzielewski@gmail.com>
2026-03-21 14:06:32 +0100

    add blood vfx on player and enemy damaged

--- src/shared/health/health_component.gd
+++ src/shared/health/health_component.gd
@@ -2,6 +2,8 @@ class_name HealthComponent
 extends Node
 
 signal health_changed(new_value: float)
+signal health_decreased()
+signal health_increased()
 signal max_health_changed(new_value: float)
 signal health_depleted
 
@@ -17,10 +19,18 @@ signal health_depleted
 
 @export var health: float = 10.0:
 	set(value):
+		var prev_health = health
 		health = clamp(value, 0, max_health)
 		if debug_log:
 			print("Health changed to ", health)
-		health_changed.emit(health)
+
+		if prev_health != health:
+			health_changed.emit(health)
+
+		if prev_health > health:
+			health_decreased.emit()
+		elif prev_health < health:
+			health_increased.emit()
 
 		if health == 0.0:
 			if debug_log:
---
+++ src/shared/vfx/player/vfx_player_sequence.gd
@@ -0,0 +1,13 @@
+@tool
+class_name VfxPlayerSequence
+extends Node3D
+
+@export_tool_button("Play") var play_action = play
+
+func play() -> void:
+	var emitters = get_children().filter(func(c): return c.get("emitting") != null)
+	for emitter in emitters:
+		emitter.emitting = true
+		await emitter.finished
+
+

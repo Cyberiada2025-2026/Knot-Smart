e2f1d787e713001f107030fb2c41e28d7a05bd27
Mikul-xyz <fmikulski@o2.pl>
2026-03-22 22:58:46 +0100

    argfoukjsfvbnilsgutybhfbziuldg

--- src/characters/player/gravity_controller.gd
+++ src/characters/player/gravity_controller.gd
@@ -38,26 +38,8 @@ func _process(delta: float) -> void:
 
 func _check_new_rotation(_delta: float) -> void:
 	if not is_rotating:
-		var getting_on_wall: bool = false
 		var floor_normal = get_sensor_normal("floor")
-		for direction in ["ui_up", "ui_down", "ui_right", "ui_left"]:
-			var sensor_normal = get_sensor_normal(direction)
-			if (
-				Input.is_action_pressed(direction) and
-				sensor_normal != null
-			):
-				new_ground_normal = sensor_normal
-				getting_on_wall = true
-				break
-			sensor_normal = get_sensor_normal("falling_"+direction)
-			if (
-				not player.player_physics.is_on_floor() and
-				sensor_normal != null and
-				floor_normal == null
-			):
-				new_ground_normal = sensor_normal
-				getting_on_wall = true
-				break
+		var getting_on_wall: bool = check_direction_senseors(floor_normal)
 		if getting_on_wall:
 			gravity_no_floor_timer.stop()
 		elif floor_normal != null:
@@ -65,6 +47,25 @@ func _check_new_rotation(_delta: float) -> void:
 			new_ground_normal = floor_normal
 		elif gravity_no_floor_timer.is_stopped():
 			gravity_no_floor_timer.start()
+
+func check_direction_senseors(floor_normal: Vector3) -> bool:
+	for direction in ["ui_up", "ui_down", "ui_right", "ui_left"]:
+		var sensor_normal = get_sensor_normal(direction)
+		if (
+			Input.is_action_pressed(direction) and
+			sensor_normal != null
+		):
+			new_ground_normal = sensor_normal
+			return true
+		sensor_normal = get_sensor_normal("falling_"+direction)
+		if (
+			not player.player_physics.is_on_floor() and
+			sensor_normal != null and
+			floor_normal == null
+		):
+			new_ground_normal = sensor_normal
+			return true
+	return false
 
 func _on_gravity_no_floor_timer_timeout() -> void:
 	new_ground_normal = Vector3.UP

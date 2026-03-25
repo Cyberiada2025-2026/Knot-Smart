d48448de65d59086151e0ea9aac56b75ac48e8d2
Titanic472 <ctac472@gmail.com>
2026-03-25 11:04:11 +0100

    wip

--- src/shared/tooltips/tooltip.gd
+++ src/shared/tooltips/tooltip.gd
@@ -9,12 +9,21 @@ extends Control
 	#if player == null:
 		#printerr("player not found, can't use tooltips without player")
 
+@export var offset: Vector2
 
 func _ready() -> void:
 	$VBoxContainer/TooltipText.text = message
-	
+	hide()
 
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		global_position = get_global_mouse_position() + offset
 
+func toggle(on: bool):
+	if on:
+		show()
+	else:
+		hide()
 #func _physics_process(_delta: float) -> void:
 	#var camera: PlayerCamera = player.get_node("PlayerPhysics/PlayerCamera")
 	#if (

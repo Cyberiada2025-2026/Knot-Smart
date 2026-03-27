488fe18afdc202337a6d4a3f170339c7059fb081
Titanic472 <ctac472@gmail.com>
2026-03-27 14:33:26 +0100

    now positioning offset works added simple style naming typos fixes in point of interest debug scene

--- src/shared/tooltips/tooltip.gd
+++ src/shared/tooltips/tooltip.gd
@@ -1,51 +1,5 @@
 class_name Tooltip
-extends Control
+extends Node
 
 @export_multiline var message: String
-#var player: Player
-#
-#func _ready() -> void:
-	#player = get_tree().get_first_node_in_group("Player")
-	#if player == null:
-		#printerr("player not found, can't use tooltips without player")
-
 @export var offset: Vector2
-
-var opacity_tween: Tween = null
-
-func _ready() -> void:
-	$VBoxContainer/TooltipText.text = message
-	hide()
-
-
-func _input(event: InputEvent) -> void:
-	if event is InputEventMouseMotion:
-		global_position = get_global_mouse_position() + offset
-
-
-func toggle(on: bool):
-	if on:
-		show()
-		modulate.a = 0.0
-		tween_opacity(1.0)
-	else: 
-		modulate.a = 1.0
-		await tween_opacity(0.0).finished
-		hide()
-		
-
-func tween_opacity(to: float):
-	if opacity_tween: 
-		opacity_tween.kill()
-	opacity_tween = get_tree().create_tween()
-	opacity_tween.tween_property(self, "modulate:a", to, 0.3)
-	return opacity_tween
-	
-
-#func _physics_process(_delta: float) -> void:
-	#var camera: PlayerCamera = player.get_node("PlayerPhysics/PlayerCamera")
-	#if (
-		#camera.get_view_type()
-		#== PlayerCamera.ViewType.FIRST_PERSON
-	#):
-		#print("found")
--- src/shared/tooltips/tooltip_manager.gd
+++ src/shared/tooltips/tooltip_manager.gd
@@ -1,20 +1,12 @@
 class_name TooltipManager
 extends Node3D
 
-#var player: Player
-
-var opacity_tween: Tween = null
-
-#delet
-#func _ready() -> void:
-	#player = get_tree().get_first_node_in_group("Player")
-	#if player == null:
-		#printerr("player not found, can't use tooltips without player")
-#/delet
-
+var vbox: VBoxContainer
+var text_container: RichTextLabel
 
 func _ready() -> void:
-	$Control.hide()
+	vbox = $Control/VBoxContainer
+	text_container = $Control/VBoxContainer/TooltipText
 	
 	
 func _physics_process(_delta: float) -> void:
@@ -28,37 +20,20 @@ func _physics_process(_delta: float) -> void:
 		raycast_result = UnsafeRaycastBuilder.new(self).enable_collisions_with_areas().raycast()
 	
 		if not raycast_result.is_empty():
-			var collider:Node3D = raycast_result.collider
+			var collider: Node3D = raycast_result.collider
 			var tooltip: Tooltip = collider.find_child("Tooltip")
-			#var distance = collider.position.distance_to(get_node("../PlayerPhysics").position)
-			#print(distance)
-			if tooltip != null:
-				#print(tooltip.message)
-				#tooltip.set_scale(Vector2(1 - distance / 10, 1 - distance / 10)) 
-				$Control/VBoxContainer/TooltipText.text = tooltip.message
+			
+			if tooltip:
+				vbox.global_position = vbox.get_global_mouse_position() + tooltip.offset
+				vbox.global_position.y -= vbox.size.y
+				text_container.text = tooltip.message
 				toggle(true)
-			#var parent = collider.ge
-			#var pos_2d = camera.uncas	
-			else:
-				toggle(false)
-		else:
-			toggle(false)
+				return
+	toggle(false)
 
 
 func toggle(on: bool):
 	if on:
 		$Control.show()
-		#$Control.modulate.a = 0.0
-		#tween_opacity(1.0)
 	else: 
-		#$Control.modulate.a = 1.0
-		#await tween_opacity(0.0).finished
 		$Control.hide()
-		
-
-func tween_opacity(to: float):
-	if opacity_tween: 
-		opacity_tween.kill()
-	opacity_tween = $Control.get_tree().create_tween()
-	opacity_tween.tween_property($Control, "modulate:a", to, 0.5)
-	return opacity_tween

3a28c1abc4600a8a224a56a6ae73466288a43540
Titanic472 <ctac472@gmail.com>
2026-03-28 09:40:45 +0100

    final final changes

--- src/shared/tooltips/tooltip_manager.gd
+++ src/shared/tooltips/tooltip_manager.gd
@@ -1,33 +1,34 @@
 class_name TooltipManager
 extends Node3D
 
-var vbox: VBoxContainer
-var text_container: RichTextLabel
+var _vbox: VBoxContainer
+var _text_container: RichTextLabel
 
 func _ready() -> void:
-	vbox = $Control/VBoxContainer
-	text_container = $Control/VBoxContainer/TooltipText
+	_vbox = $Control/VBoxContainer
+	_text_container = $Control/VBoxContainer/TooltipText
 	process_mode = Node.PROCESS_MODE_ALWAYS
-	
-	
+
+
 func _physics_process(_delta: float) -> void:
-	var raycast_result
 	var camera = get_node("../PlayerPhysics/PlayerCamera")
-	
+
 	if (
 		camera.get_view_type() == PlayerCamera.ViewType.FIRST_PERSON
 		and not get_tree().paused
 	):
-		raycast_result = UnsafeRaycastBuilder.new(self).enable_collisions_with_areas().raycast()
-	
+		var raycast_result = (
+			UnsafeRaycastBuilder.new(self).enable_collisions_with_areas().raycast()
+		)
+
 		if not raycast_result.is_empty():
 			var collider: Node3D = raycast_result.collider
 			var tooltip: Tooltip = collider.find_child("Tooltip")
-			
+
 			if tooltip:
-				vbox.global_position = vbox.get_global_mouse_position() + tooltip.offset
-				vbox.global_position.y -= vbox.size.y
-				text_container.text = tooltip.message
+				_vbox.global_position = _vbox.get_global_mouse_position() + tooltip.offset
+				_vbox.global_position.y -= _vbox.size.y
+				_text_container.text = tooltip.message
 				toggle(true)
 				return
 	toggle(false)
@@ -36,5 +37,5 @@ func _physics_process(_delta: float) -> void:
 func toggle(on: bool):
 	if on:
 		$Control.show()
-	else: 
+	else:
 		$Control.hide()

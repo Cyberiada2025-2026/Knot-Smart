538b2c2cf6d59b7c0f344f34d1c2512f40df208d
Natalia <nata18629@gmail.com>
2026-04-02 15:34:29 +0200

    Serialize trees

--- src/shared/tree_generator/tree_generator.gd
+++ src/shared/tree_generator/tree_generator.gd
@@ -2,8 +2,7 @@
 class_name TreeGenerator
 extends Node3D
 
-
-const TEX_DARKEN = 0.5
+const DIR_PATH = "user://trees"
 
 @export_tool_button("Generate", "Callable") var generate_button = on_generate
 @export var params: TreeParameters
@@ -11,6 +10,7 @@ const TEX_DARKEN = 0.5
 var tree_skeleton: TreeSkeleton
 var tree_mesh_generator: TreeMeshGenerator
 var tree: StaticBody3D
+var tree_scene: PackedScene
 
 
 func _ready() -> void:
@@ -23,22 +23,19 @@ func _ready() -> void:
 
 func generate_tree():
 	tree = StaticBody3D.new()
-	add_child(tree)
+	tree.name = "tree"
 	tree_skeleton.params = params
 	var branches_one_level: Array[TreeBranch] = []
 	for i in range(params.branch_recursion_level+1): # levels of branches + trunk
 		branches_one_level = tree_skeleton.generate_skeleton(branches_one_level)
 		for branch in branches_one_level:
 			generate_mesh(branch, params.material)
+	serialize()
 
 
 func generate_mesh(branch: TreeBranch, material: StandardMaterial3D):
 	var mesh = MeshInstance3D.new()
 	var array_mesh = tree_mesh_generator.generate_array_mesh(branch)
-	#material.uv1_triplanar = true
-	#material.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
-	#material.albedo_texture = texture
-	#material.albedo_color *= TEX_DARKEN
 	array_mesh.surface_set_material(0, material)
 	mesh.mesh = array_mesh
 	mesh.transform = branch.transform
@@ -50,14 +47,27 @@ func generate_mesh(branch: TreeBranch, material: StandardMaterial3D):
 	collision.transform = branch.transform
 	tree.add_child(collision)
 	if Engine.is_editor_hint():
-		tree.owner = get_tree().edited_scene_root
-		mesh.owner = get_tree().edited_scene_root
-		collision.owner = get_tree().edited_scene_root
+		mesh.owner = tree
+		collision.owner = tree
 
 
 func on_generate():
+	tree_scene = PackedScene.new()
 	tree_skeleton.rec_level = 0
 	for child in get_children():
 		if child is StaticBody3D:
 			child.queue_free()
 	generate_tree()
+
+
+func serialize():
+	var result = tree_scene.pack(tree)
+	if result == OK:
+		if not DirAccess.dir_exists_absolute(DIR_PATH):
+			DirAccess.make_dir_absolute(DIR_PATH)
+		var error = ResourceSaver.save(
+			tree_scene,
+			DIR_PATH + "/tree%d.tscn" % tree.get_rid().get_id())
+		if error != OK:
+			push_error("An error occurred while saving the scene to disk.")
+		add_child(tree_scene.instantiate())

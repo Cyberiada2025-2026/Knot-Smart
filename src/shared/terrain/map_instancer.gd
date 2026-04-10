1093edf2b45b0976bebaade978eae5d03b32cbb3
alanva36 <alanva36official@gmail.com>
2026-04-10 23:12:28 +0200

    reworked terrain generation to use mesh, map saves to files, fixed chunk manager and structure of project

--- src/shared/terrain/map_instancer.gd
+++ src/shared/terrain/map_instancer.gd
@@ -10,6 +10,7 @@ var world_generation_params: WorldGenerationParams
 var world_display_params: WorldDisplayParams
 var blueprint: MapTileData
 
+var shape_cache: Dictionary = {}
 
 func _init(manager: MapRenderer):
 	blueprint = manager.blueprint
@@ -48,78 +49,64 @@ func create_map_instance(MAP_DIR: String = SCENE_DIR) -> void:
 
 
 func create_chunk_scene(chunk_coord: Vector2i, CHUNK_PATH: String) -> void:
-	
 	var chunk_node = Node3D.new()
 	chunk_node.name = "ChunkX%dZ%d" % [chunk_coord.x, chunk_coord.y]
-
-	# Godot requires to add node to tree before modifying it
-
 	chunk_node.position = Vector3(
 		chunk_coord.x * world_generation_params.get_chunk_unit_size(),
 		0,
 		chunk_coord.y * world_generation_params.get_chunk_unit_size()
 	)
 
-	var chunk_mesh_instance = MeshInstance3D.new()
-	chunk_mesh_instance.name = "ChunkMesh"
-	chunk_node.add_child(chunk_mesh_instance)
-	chunk_mesh_instance.owner = chunk_node
-
-	chunk_mesh_instance.mesh = generate_chunk_mesh(chunk_coord)
-
-	if world_display_params.terrain_material:
-		chunk_mesh_instance.material_override = world_display_params.terrain_material
-
-	chunk_mesh_instance.create_trimesh_collision()
-	for child in chunk_mesh_instance.get_children():
-		child.owner = chunk_node
-		for grand_child in child.get_children():
-			grand_child.owner = chunk_node
+	var mesh_groups: Dictionary = {}
+	
+	var chunk_start = chunk_coord * world_generation_params.chunk_size
+	for x in world_generation_params.chunk_size:
+		for z in world_generation_params.chunk_size:
+			var world_coord = chunk_start + Vector2i(x, z)
+			if not blueprint.data.has(world_coord): continue
 			
+			var tile_info = blueprint.data[world_coord]
+			for data_node in tile_info.objects:
+				var m = data_node.mesh
+				if not mesh_groups.has(m):
+					mesh_groups[m] = []
+				
+				var pos = Vector3(x * world_generation_params.tile_size, tile_info.height, z * world_generation_params.tile_size)
+				var t = Transform3D(data_node.basis, pos)
+				mesh_groups[m].append(t)
+
+	for mesh_key in mesh_groups.keys():
+		var transforms = mesh_groups[mesh_key]
+		
+		var mmi = MultiMeshInstance3D.new()
+		mmi.multimesh = MultiMesh.new()
+		mmi.multimesh.transform_format = MultiMesh.TRANSFORM_3D
+		mmi.multimesh.mesh = mesh_key
+		mmi.multimesh.instance_count = transforms.size()
+		
+		for i in range(transforms.size()):
+			mmi.multimesh.set_instance_transform(i, transforms[i])
+			
+		chunk_node.add_child(mmi)
+		mmi.owner = chunk_node
+		
+		if world_display_params.terrain_material:
+			mmi.material_override = world_display_params.terrain_material
+		
+		if not shape_cache.has(mesh_key):
+			shape_cache[mesh_key] = mesh_key.create_trimesh_shape()
+		for t in transforms:
+			var sb = StaticBody3D.new()
+			var col = CollisionShape3D.new()
+			col.shape = shape_cache[mesh_key]
+			
+			sb.add_child(col)
+			mmi.add_child(sb)
+			
+			sb.transform = t
+			sb.owner = chunk_node
+			col.owner = chunk_node
+
 	var scene = PackedScene.new()
-	scene.take_over_path(CHUNK_PATH)
-	var result = scene.pack(chunk_node)
-	if result == OK:
-		var error = ResourceSaver.save(scene, CHUNK_PATH)
-		if error != OK:
-			push_error("An error occurred while saving chunk to disk.")
-
-
-func generate_chunk_mesh(chunk_coord: Vector2i) -> Mesh:
-	var st = SurfaceTool.new()
-	st.begin(Mesh.PRIMITIVE_TRIANGLES)
-
-	var chunk_size = world_generation_params.chunk_size
-	var tile_size = world_generation_params.tile_size
-
-	for x in chunk_size:
-		for z in chunk_size:
-			var vertices: Array[Vector3] = []
-			for i in 2:
-				for j in 2:
-					var vertex_position = Vector3(x + j, 0, z + i) * tile_size
-					var height_sample_position = chunk_coord * chunk_size + Vector2i(x, z)
-					vertex_position.y = blueprint.get_height(
-						height_sample_position + Vector2i(j, i)
-					)
-					vertices.push_back(vertex_position)
-
-			add_quad(st, vertices)
-	# Finalize mesh
-	st.generate_tangents()
-	return st.commit()
-
-
-func add_triangle(st: SurfaceTool, vertices: Array):
-	var normal = ((vertices[2] - vertices[0]).cross(vertices[1] - vertices[0])).normalized()
-	for v in vertices:
-		var uv = Vector2(v.x, v.z) / float(world_generation_params.tile_size)
-
-		st.set_normal(normal)
-		st.set_uv(uv)
-		st.add_vertex(v)
-
-
-func add_quad(st: SurfaceTool, vertices: Array[Vector3]):
-	add_triangle(st, [vertices[0], vertices[1], vertices[3]])
-	add_triangle(st, [vertices[0], vertices[3], vertices[2]])
+	scene.pack(chunk_node)
+	ResourceSaver.save(scene, CHUNK_PATH)
--- src/shared/terrain/terrain_generator.gd
+++ src/shared/terrain/terrain_generator.gd
@@ -5,51 +5,71 @@ extends Node
 var world_generation_params: WorldGenerationParams
 var blueprint: MapTileData
 
+@export var mesh_flat: Mesh
+@export var mesh_slope: Mesh
+@export var mesh_corner: Mesh
 
 func run_generation(manager: GridGenerationPipeline) -> void:
 	blueprint = manager.blueprint
 	world_generation_params = manager.world_generation_params
 
+	var get_step = func(tx: int, tz: int):
+		var raw = world_generation_params.noise.get_noise_2d(tx, tz)
+		var norm = (raw + 1) / 2.0
+		return floor((norm + world_generation_params.height_displacement) * world_generation_params.map_height)
+
 	for x in blueprint.world_size:
 		for z in blueprint.world_size:
 			var coord = Vector2i(x, z)
-
-			var get_step = func(tx: int, tz: int):
-				var raw = world_generation_params.noise.get_noise_2d(tx, tz)
-				var norm = (raw + 1) / 2.0
-				return floor((norm + world_generation_params.height_displacement) * world_generation_params.map_height)
-
 			var current_step = get_step.call(x, z)
-
-			var h_n = get_step.call(x, z - 1) # North
-			var h_s = get_step.call(x, z + 1) # South
-			var h_e = get_step.call(x + 1, z) # East
-			var h_w = get_step.call(x - 1, z) # West
+			
+			var final_height = current_step * world_generation_params.tile_height
+			
+			var h_n = get_step.call(x, z - 1)
+			var h_s = get_step.call(x, z + 1)
+			var h_e = get_step.call(x + 1, z)
+			var h_w = get_step.call(x - 1, z)
 
 			var up_n = h_n > current_step
 			var up_s = h_s > current_step
 			var up_e = h_e > current_step
 			var up_w = h_w > current_step
 
-			var mesh_to_use : Mesh
-			var mesh_rotation : float = 0.0
+			var selected_mesh : Mesh = mesh_flat
+			var rotation_y : float = 0.0
+			var rule = TileInfo.PlacementRule.FLAT
 
-			if (up_n or up_s) and (up_e or up_w):
-				mesh_to_use = assets.mesh_corner
-				if up_n and up_e: mesh_rotation = PI / 2
-				elif up_e and up_s: mesh_rotation = PI
-				elif up_s and up_w: mesh_rotation = 1.5 * PI
-				else: mesh_rotation = 0.0
-				
-			elif up_n or up_s or up_e or up_w:
-				mesh_to_use = 
-				# Example rotation logic for a slope
-				if up_n: mesh_rotation = 0.0
-				elif up_e: mesh_rotation = PI / 2
-				elif up_s: mesh_rotation = PI
-				elif up_w: mesh_rotation = 1.5 * PI
-				
+			var is_horizontal = up_e or up_w
+			var is_vertical = up_n or up_s
+
+			if is_horizontal and is_vertical:
+				selected_mesh = mesh_corner
+				rule = TileInfo.PlacementRule.BLOCKED
+			elif is_horizontal or is_vertical:
+				selected_mesh = mesh_slope
+				rule = TileInfo.PlacementRule.SLOPE_X if is_horizontal else TileInfo.PlacementRule.SLOPE_Z
 			else:
-				mesh_to_use = assets.mesh_flat
+				selected_mesh = mesh_flat
+				rule = TileInfo.PlacementRule.FLAT
 
-			blueprint.data[coord].height = final_height
+			if selected_mesh == mesh_corner:
+				if up_n and up_e:   rotation_y = PI / 2     # NE
+				elif up_e and up_s: rotation_y = PI          # SE
+				elif up_s and up_w: rotation_y = 1.5 * PI    # SW
+				else:               rotation_y = 0.0         # NW
+			elif selected_mesh == mesh_slope:
+				if up_e:    rotation_y = PI / 2
+				elif up_s:  rotation_y = PI
+				elif up_w:  rotation_y = 1.5 * PI
+				else:       rotation_y = 0.0
+			
+			var mi = MeshInstance3D.new()
+			mi.mesh = selected_mesh
+			mi.rotation.y = rotation_y
+			mi.position = Vector3.ZERO 
+
+			var tile = blueprint.data[coord]
+			tile.height = final_height
+			tile.placement_rule = rule
+			tile.objects.clear()
+			tile.objects.append(mi)

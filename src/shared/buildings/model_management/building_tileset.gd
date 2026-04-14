a13c4c1f747857e2884ca9191b2559fef76c32d9
ThreeCoffes <leonard.madzielewski@gmail.com>
2026-04-14 15:47:42 +0200

    remove unused files

--- src/shared/buildings/model_management/building_tileset.gd
+++
@@ -1,8 +0,0 @@
-@tool
-class_name BuildingTileset
-extends Resource
-
-@export var mesh_library: MeshLibrary
-@export var grid_size: Vector3i = Vector3i.ONE
-## Mesh Libraries made for eneterable buildings should have their own collision setup.
-@export var is_enterable: bool = true

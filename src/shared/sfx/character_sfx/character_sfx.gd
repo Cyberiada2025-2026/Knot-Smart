7b2d5fcaa7746a2a732ecc226913cc2836370536
Thedoczek <dokmiada@gmail.com>
2026-04-26 16:35:12 +0200

    Fix linting for basic footstep version

--- src/shared/sfx/character_sfx/character_sfx.gd
+++ src/shared/sfx/character_sfx/character_sfx.gd
@@ -2,5 +2,6 @@ extends Node3D
 
 var character: CharacterBody3D
 
+
 func _ready() -> void:
 	character = self.get_parent()
--- src/shared/sfx/character_sfx/footsteps_sfx.gd
+++ src/shared/sfx/character_sfx/footsteps_sfx.gd
@@ -1,13 +1,15 @@
 extends AudioStreamPlayer3D
 
+@export var step_interval: float = 0.3
+
 var character: CharacterBody3D
 var time_since_last_step: float = 0
 
-@export var step_interval: float = 0.3
 
 func _ready() -> void:
 	character = get_node("../../")
 	print(character)
+
 
 func _process(delta: float) -> void:
 	time_since_last_step += delta

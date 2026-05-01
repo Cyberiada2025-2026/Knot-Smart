0a0d1358eacf1d304f6d755761f1ae4e1037bdd2
Natalia <nata18629@gmail.com>
2026-05-01 14:22:51 +0200

    Separated serialization logic to a different script

--- src/shared/nature_generators/foliage_generator/foliage_generator.gd
+++ src/shared/nature_generators/foliage_generator/foliage_generator.gd
@@ -2,13 +2,11 @@
 class_name FoliageGenerator
 extends Node3D
 
+const DIR_PATH = "user://foliage"
+
 var foliage: StaticBody3D
 var foliage_scene: PackedScene
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func serialize():
+	add_child(Serialize.serialize(foliage_scene, foliage, DIR_PATH))
--- src/shared/nature_generators/foliage_generator/foliage_parameters.gd
+++ src/shared/nature_generators/foliage_generator/foliage_parameters.gd
@@ -2,3 +2,4 @@ class_name FoliageParameters
 extends Resource
 
 @export var material: StandardMaterial3D
+@export_range(1, 5, 1) var count: int = 2

b3ec64bdab250098373f0b26ea57cf089587510b
Natalia <nata18629@gmail.com>
2026-05-01 15:23:36 +0200

    Working base of foliage generator

--- src/shared/nature_generators/foliage_generator/foliage_generator.gd
+++ src/shared/nature_generators/foliage_generator/foliage_generator.gd
@@ -4,8 +4,38 @@ extends Node3D
 
 const DIR_PATH = "user://foliage"
 
+@export_tool_button("Generate", "Callable") var generate_button = on_generate
+@export var params: FoliageParameters
+
 var foliage: StaticBody3D
 var foliage_scene: PackedScene
+
+
+func _ready() -> void:
+	on_generate()
+
+
+func generate_foliage():
+	foliage = StaticBody3D.new()
+	foliage.name = "foliage"
+	var angle = PI/params.count
+	for i in range(params.count):
+		var mesh = MeshInstance3D.new()
+		mesh.mesh = PlaneMesh.new()
+		mesh.rotate_z(angle*i)
+		mesh.rotate_x(PI/2)
+		mesh.mesh.surface_set_material(0, params.material)
+		foliage.add_child(mesh)
+		mesh.owner = foliage
+	serialize()
+
+
+func on_generate():
+	foliage_scene = PackedScene.new()
+	for child in get_children():
+		if child is StaticBody3D:
+			child.queue_free()
+	generate_foliage()
 
 
 func serialize():

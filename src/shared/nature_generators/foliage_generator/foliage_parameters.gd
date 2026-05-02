c2749bd23e82128e6baffead500aab69c3ce6aef
Natalia <nata18629@gmail.com>
2026-05-02 14:04:33 +0200

    Changing flat branches scale works fine now + file structure change

--- src/shared/nature_generators/foliage_generator/foliage_parameters.gd
+++ src/shared/nature_generators/foliage_generator/foliage_parameters.gd
@@ -3,5 +3,9 @@ extends Resource
 
 @export var material: StandardMaterial3D
 @export_range(1, 5, 1) var count: int = 2
+@export_range(0.5, 5.0, 0.5) var scale: float = 1.0
+@export_range(0.5, 5.0, 0.5) var scale_randomization: float = 1.0
+
+@export_group("DEBUG")
 @export var plane_scale: Vector3 = Vector3.ONE
 @export var plane_offset = Vector3.ZERO

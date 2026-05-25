64a9c759961b28d87d2eceec8d98a389005bd923
Natalia <nata18629@gmail.com>
2026-05-25 13:42:08 +0200

    Play walk and idle animations

--- src/characters/player/player_main.gd
+++ src/characters/player/player_main.gd
@@ -10,6 +10,11 @@ extends CharacterBody3D
 @export var slowing_speed = 500.0
 @export var jump_strength = 9.5
 @export var gravity_strength = 19.0
+var animation_player: AnimationPlayer
+
+
+func _ready() -> void:
+	animation_player = player_model.find_child("AnimationPlayer", false)
 
 
 func _on_player_camera_camera_rotated(_vector: Vector3, angle: float) -> void:
@@ -73,9 +78,11 @@ func _handle_flat_movement(delta: float) -> void:
 func _handle_move_input(delta: float):
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down").normalized()
 	if input_dir:
+		animation_player.play("Walk")
 		velocity.x = -input_dir.x * speed * delta
 		velocity.z = -input_dir.y * speed * delta
 	else:
+		animation_player.play("Idle")
 		velocity.x = move_toward(velocity.x, 0, slowing_speed * delta)
 		velocity.z = move_toward(velocity.z, 0, slowing_speed * delta)
 

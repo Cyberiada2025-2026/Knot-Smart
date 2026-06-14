0644111c5734d0e5b03d9f4e671d2ba912353035
Natalia <nata18629@gmail.com>
2026-06-14 17:39:35 +0200

    Smooth animation transitioning

--- src/characters/player/player_main.gd
+++ src/characters/player/player_main.gd
@@ -1,6 +1,8 @@
 class_name Player
 extends CharacterBody3D
 
+enum {IDLE, WALK}
+
 @export_category("MODULES")
 @export var player_model: Node3D
 @export var player_camera: PlayerCamera
@@ -10,11 +12,15 @@ extends CharacterBody3D
 @export var slowing_speed = 500.0
 @export var jump_strength = 9.5
 @export var gravity_strength = 19.0
-var animation_player: AnimationPlayer
+@export var blend_anim_speed = 0.5
+
+var animation_tree: AnimationTree
+var walk_blend = 0.0
+var curr_anim = IDLE
 
 
 func _ready() -> void:
-	animation_player = player_model.find_child("AnimationPlayer", false)
+	animation_tree = player_model.find_child("AnimationTree", false)
 
 
 func _on_player_camera_camera_rotated(_vector: Vector3, angle: float) -> void:
@@ -78,11 +84,11 @@ func _handle_flat_movement(delta: float) -> void:
 func _handle_move_input(delta: float):
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down").normalized()
 	if input_dir:
-		animation_player.play("Walk")
+		animation_tree.set("parameters/Movement/transition_request", "Walk")
 		velocity.x = -input_dir.x * speed * delta
 		velocity.z = -input_dir.y * speed * delta
 	else:
-		animation_player.play("Idle")
+		animation_tree.set("parameters/Movement/transition_request", "Idle")
 		velocity.x = move_toward(velocity.x, 0, slowing_speed * delta)
 		velocity.z = move_toward(velocity.z, 0, slowing_speed * delta)
 

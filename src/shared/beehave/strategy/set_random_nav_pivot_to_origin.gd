c6067b907823a4c40859c97feb536c2496fb00c7
Valk0x29a <62944006+valk0x29a@users.noreply.github.com>
2026-08-02 18:45:55 +0200

    small fix

--- src/shared/beehave/strategy/set_random_nav_pivot_to_origin.gd
+++ src/shared/beehave/strategy/set_random_nav_pivot_to_origin.gd
@@ -7,6 +7,7 @@ extends Node
 func _ready() -> void:
 	var random_nav: SetRandomNavTarget = get_node("..")
 	if random_nav == null:
-		printerr("No SetRandomNavTarget Found!!!")
+		printerr("No SetRandomNavTarget found in Parent!!!")
+		return
 	random_nav.random_point_around_player = false
 	random_nav.random_point_pivot = transform_node.global_position

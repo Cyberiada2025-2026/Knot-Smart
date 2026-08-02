814f16fca3b1f021cbba611fa2f7a419f541bf9b
Wiktoria Tomczak <s203283@student.pg.edu.pl>
2026-08-02 19:44:05 +0200

    new PointOfInterest

--- src/shared/point_of_interest/point_of_interest.gd
+++ src/shared/point_of_interest/point_of_interest.gd
@@ -4,56 +4,40 @@ extends Node3D
 
 signal triggered(entity: Node3D)
 
-const VISUALIZATION_MATERIAL = preload(
-	"res://shared/point_of_interest/point_of_interest_visualization_material.tres"
-)
-
-@export var radius: float = 0.5:
-	set(value):
-		radius = value
-		collider.shape.radius = radius
-		visualization_mesh.mesh.radius = radius
-		visualization_mesh.mesh.height = radius * 2
-
-@export var visualize: bool = true:
-	set(value):
-		visualize = value
-		visualization_mesh.visible = visualize
-
+@export var radius: float = 0.5
+@export var visualize: bool = true
 @export var trigger_group_name: StringName = "Player"
 
-var area
-var collider
-var visualization_mesh
+var area: Area3D
 
 
 func _init():
 	area = Area3D.new()
-	collider = CollisionShape3D.new()
-	visualization_mesh = MeshInstance3D.new()
-
 	add_child(area)
 	area.body_entered.connect(_on_area_3d_body_entered)
 
-	area.add_child(collider)
+	var collider = CollisionShape3D.new()
 	collider.shape = SphereShape3D.new()
-
-	area.add_child(visualization_mesh)
-	visualization_mesh.mesh = SphereMesh.new()
-	visualization_mesh.mesh.material = VISUALIZATION_MATERIAL
-
-
-func _ready():
-	child_order_changed.connect(_can_queue_free)
+	collider.shape.radius = radius
+	area.add_child(collider)
+	
+	if visualize:
+		var mesh = MeshInstance3D.new()
+		mesh.mesh = SphereMesh.new()
+		mesh.mesh.radius = radius
+		mesh.mesh.height = radius * 2
+		
+		var material = StandardMaterial3D.new()
+		material.transparency = BaseMaterial3D.TRANSPARENCY_ALPHA
+		material.albedo_color = Color(0.5, 0.9, 0.9, 0.2)
+		mesh.mesh.material = material
+		
+		area.add_child(mesh)
 
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body.is_in_group(trigger_group_name):
 		triggered.emit(body)
 		area.queue_free()
-		_can_queue_free()
-
-
-func _can_queue_free():
-	if get_child_count() == 0:
-		queue_free()
+		if get_child_count() == 0:
+			queue_free()

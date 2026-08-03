1a8cfd22ea0da883cea06c6296e73ff33e954e80
Valk0x29a <62944006+valk0x29a@users.noreply.github.com>
2026-08-03 12:34:34 +0200

    formatter

--- src/characters/npcs/pig.gd
+++ src/characters/npcs/pig.gd
@@ -15,7 +15,7 @@ var should_track_target: bool = false
 
 func _ready() -> void:
 	world = Engine.get_main_loop().root.get_world_3d()
-	navigation_agent_3d.set_target_position(global_position);
+	navigation_agent_3d.set_target_position(global_position)
 
 
 func get_point_on_map(point: Vector3) -> Vector3:

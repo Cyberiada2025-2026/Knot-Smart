4069dd145fed2712956b24ce53ecf3fa9340e390
Lenetis <70149832+Lenetis@users.noreply.github.com>
2026-08-07 21:07:49 +0200

    Change ImmediateControlStrategy to state machine

--- src/characters/player/rope/strategies/control/immediate_control_strategy.gd
+++ src/characters/player/rope/strategies/control/immediate_control_strategy.gd
@@ -1,7 +1,10 @@
 extends ControlStrategyInterface
 
+enum State { NO_ROPE, ROPE_EXISTS }
+
 @export var rope_params = RopeParams.new()
 
+var state = State.NO_ROPE
 var sphere: MeshInstance3D = preload("uid://ymb8m1pspwfy").instantiate()
 var active_rope: Rope = null
 
@@ -11,29 +14,33 @@ func use_rope(raycast_result: Dictionary) -> void:
 		return
 		
 	var player = get_node("../..")
-
-	if active_rope:
-		active_rope.change_attach_node(
-			player,
-			raycast_result.collider,
-			ControlStrategyUtilities.create_marker(
-				raycast_result.collider, raycast_result.position, sphere
+	
+	match state:
+		State.NO_ROPE:
+			var target_marker = ControlStrategyUtilities.create_marker_from_unsafe_raycast(
+				raycast_result, sphere
 			)
-		)
-		active_rope = null
 
-	else:
-		var target_marker = ControlStrategyUtilities.create_marker_from_unsafe_raycast(
-			raycast_result, sphere
-		)
+			var player_marker = ControlStrategyUtilities.create_marker_on_player(player, sphere)
 
-		var player_marker = ControlStrategyUtilities.create_marker_on_player(player, sphere)
-
-		active_rope = ControlStrategyUtilities.create_rope(
-			rope_params, [raycast_result.collider, player], [target_marker, player_marker]
-		)
-		active_rope.finished.connect(_on_active_rope_finished)
-		add_child(active_rope)
+			active_rope = ControlStrategyUtilities.create_rope(
+				rope_params, [raycast_result.collider, player], [target_marker, player_marker]
+			)
+			active_rope.finished.connect(_on_active_rope_finished)
+			add_child(active_rope)
+			state = State.ROPE_EXISTS
+			
+		State.ROPE_EXISTS:
+			print("Yes rope")
+			active_rope.change_attach_node(
+				player,
+				raycast_result.collider,
+				ControlStrategyUtilities.create_marker(
+					raycast_result.collider, raycast_result.position, sphere
+				)
+			)
+			active_rope = null
+			state = State.NO_ROPE
 
 
 func break_rope(raycast_result: Dictionary) -> void:
@@ -58,3 +65,4 @@ func select_player(_raycast_result: Dictionary) -> void:
 
 func _on_active_rope_finished():
 	active_rope = null
+	state = State.NO_ROPE

417a25bda90f4c294627b50333487f7886673354
Valk0x29a <62944006+valk0x29a@users.noreply.github.com>
2026-08-07 20:55:34 +0200

    formatter

--- src/shared/beehave/action/set_random_nav_target.gd
+++ src/shared/beehave/action/set_random_nav_target.gd
@@ -2,8 +2,9 @@
 class_name SetRandomNavTarget
 extends ActionLeaf
 
-@export var set_random_target_strategy: SetRandomNavPivotInterface;
+@export var set_random_target_strategy: SetRandomNavPivotInterface
+
 
 func tick(actor: Node, _blackboard: Blackboard) -> int:
-	set_random_target_strategy.set_random_nav_target(actor);
+	set_random_target_strategy.set_random_nav_target(actor)
 	return SUCCESS
